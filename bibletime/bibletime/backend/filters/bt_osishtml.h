223eb2564fffbfb55d4bdf1cf7994ba27a9989a3
eelik <eelik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2007-08-03 11:12:09 +0000

    Moved filters into new subdir
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@178 ba6c7691-6512-0410-bf18-b6da21b6447f

---
+++ bibletime/bibletime/backend/filters/bt_osishtml.h
@@ -0,0 +1,65 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2006 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef BT_OSISHTML_H
+#define BT_OSISHTML_H
+
+//Sword includes
+#include <swbuf.h>
+#include <osishtmlhref.h>
+
+namespace Filters {
+
+	/** BibleTime's OSIS to HTMl filter.
+	* This filter works on OSIS tags and outputs HTML in the structure supported by BibleTime.
+	*/
+
+class BT_OSISHTML : public sword::OSISHTMLHREF {
+
+protected:
+
+class BT_UserData : public sword::OSISHTMLHREF::MyUserData {
+
+public:
+BT_UserData(const sword::SWModule *module, const sword::SWKey *key) : sword::OSISHTMLHREF::MyUserData(module, key) {
+				noteType = Unknown;
+				swordFootnote = 1;
+				inCrossrefNote = false;
+			}
+
+			unsigned short int swordFootnote;
+			bool inCrossrefNote;
+
+			enum NoteType {
+				Unknown,
+				Alternative,
+				CrossReference,
+				Footnote,
+				StrongsMarkup
+			} noteType;
+
+			struct {
+				sword::SWBuf who;
+			}
+
+			quote;
+		};
+
+		virtual sword::BasicFilterUserData *createUserData(const sword::SWModule* module, const sword::SWKey* key) {
+			return new BT_UserData(module, key);
+		}
+
+public:
+		BT_OSISHTML();
+		virtual bool handleToken(sword::SWBuf &buf, const char *token, sword::BasicFilterUserData *userData);
+	};
+
+} //end of Filters namespace
+
+#endif
---
+++ bibletime/bibletime/backend/filters/bt_plainhtml.h
@@ -0,0 +1,33 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2006 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef BT_PLAINHTML_H
+#define BT_PLAINHTML_H
+
+//Sword includes
+#include <swfilter.h>
+#include <swbuf.h>
+
+class SWKey;
+class SWModule;
+
+namespace Filters {
+
+	/** Plain to HTML filter,
+	* This filter converts Plain Text into HTML
+	*/
+class BT_PLAINHTML : public sword::SWFilter{
+protected:
+		virtual char processText(sword::SWBuf& buf, const sword::SWKey*, const sword::SWModule * = 0);
+public:
+		BT_PLAINHTML();
+	};
+}
+
+#endif
