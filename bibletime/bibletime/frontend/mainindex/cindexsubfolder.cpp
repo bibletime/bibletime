a08270c43ad9662ed2284722e7d35002493cd8ce
eelik <eelik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2007-08-04 16:21:38 +0000

    Ported cindexsubfolder.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@199 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/bibletime/frontend/mainindex/cindexsubfolder.cpp
+++ bibletime/bibletime/frontend/mainindex/cindexsubfolder.cpp
@@ -9,65 +9,86 @@
 // Copyright: See COPYING file that comes with this distribution
 //
 //
-SubFolder::SubFolder(CFolderBase* parentItem, const QString& caption) : CBookmarkFolder(parentItem, BookmarkFolder) {
-		m_startupXML = QDomElement();
-		setText( 0, caption );
-	}
 
-SubFolder::SubFolder(CFolderBase* parentItem, QDomElement& xml ) : CBookmarkFolder(parentItem, BookmarkFolder) {
-		m_startupXML = xml;
-	}
+#include "cindexsubfolder.h"
+#include "cindexfolderbase.h"
+#include "cindexbookmarkfolder.h"
+#include "cindexbookmarkitem.h"
 
-	SubFolder::~SubFolder() {}
+#include <QtXml/qdom.h>
 
-	void SubFolder::init() {
-		CFolderBase::init();
-		if (!m_startupXML.isNull())
-			loadFromXML(m_startupXML);
 
-		setDropEnabled(true);
-		setRenameEnabled(0,true);
-	}
+CIndexSubFolder::CIndexSubFolder(CIndexFolderBase* parentItem, const QString& caption)
+	: CIndexBookmarkFolder(parentItem, BookmarkFolder)
+{
+	m_startupXML = QDomElement();
+	setText( 0, caption );
+}
 
-	/** Reimplementation from  CItemBase. */
-	const bool SubFolder::enableAction(const MenuAction action) {
-		if (action == ChangeFolder || action == NewFolder || action == DeleteEntries || action == ImportBookmarks )
-			return true;
+CIndexSubFolder::CIndexSubFolder(CIndexFolderBase* parentItem, QDomElement& xml )
+	: CIndexBookmarkFolder(parentItem, BookmarkFolder)
+{
+	m_startupXML = xml;
+}
 
-		if (action == ExportBookmarks || action == ImportBookmarks )
-			return true; //not yet implemented
+CIndexSubFolder::~CIndexSubFolder() {}
 
-		if ((action == PrintBookmarks) && childCount())
-			return true;
+void CIndexSubFolder::init() {
+	CIndexFolderBase::init();
+	if (!m_startupXML.isNull())
+		loadFromXML(m_startupXML);
+	//setDropEnabled(true);
+	//setRenameEnabled(0,true); //TODO
+}
 
-		return false;
-	}
+/** Reimplementation from  CItemBase. */
+const bool CIndexSubFolder::enableAction(const MenuAction action) {
+	if (action == ChangeFolder || action == NewFolder || action == DeleteEntries || action == ImportBookmarks )
+		return true;
+	if (action == ExportBookmarks || action == ImportBookmarks )
+		return true; //not yet implemented
+	if ((action == PrintBookmarks) && childCount())
+		return true;
+	return false;
+}
 
-	/** Returns the XML code which represents the content of this folder. */
-	QDomElement SubFolder::saveToXML( QDomDocument& doc ) {
-		/**
-		* Save all subitems (bookmarks and folders) to the XML file.
-		* We get the XML code for the items by calling their own saveToXML implementations.
-		*/
-		QDomElement elem = doc.createElement("Folder");
-		elem.setAttribute("caption", text(0));
+/** Returns the XML code which represents the content of this folder. */
+QDomElement CIndexSubFolder::saveToXML( QDomDocument& doc ) {
+	/**
+	* Save all subitems (bookmarks and folders) to the XML file.
+	* We get the XML code for the items by calling their own saveToXML implementations.
+	*/
+	QDomElement elem = doc.createElement("Folder");
+	elem.setAttribute("caption", text(0));
 
-		//append the XML nodes of all child items
-		CItemBase* i = dynamic_cast<CItemBase*>(firstChild());
-		while( i ) {
-			if (i->parent() == this) {
-				QDomElement newElem = i->saveToXML( doc );
-				if (!newElem.isNull()) {
-					elem.appendChild( newElem ); //append to this folder
-				}
+	//append the XML nodes of all child items
+	//CIndexItemBase* i = dynamic_cast<CIndexItemBase*>(firstChild());
+	//while( i ) {
+	//	if (i->parent() == this) {
+	//		QDomElement newElem = i->saveToXML( doc );
+	//		if (!newElem.isNull()) {
+	//			elem.appendChild( newElem ); //append to this folder
+	//		}
+	//	}
+	//	i = dynamic_cast<CItemBase*>( i->nextSibling() );
+	//}
+	
+	for(int n = 0; n < childCount(); n++) {
+		CIndexItemBase* i = dynamic_cast<CIndexItemBase*>( child(n) );
+		if (i->parent() == this) { //only one level under this folder
+			QDomElement newElem = i->saveToXML( doc ); // the cild creates it's own XML code
+			if (!newElem.isNull()) {
+				elem.appendChild( newElem ); //append to this folder
 			}
-			i = dynamic_cast<CItemBase*>( i->nextSibling() );
 		}
-		return elem;
+		
 	}
+
+	return elem;
+}
 
 	/** Loads the content of this folder from the XML code passed as argument to this function. */
-	void SubFolder::loadFromXML( QDomElement& elem ) {
+	void CIndexSubFolder::loadFromXML( QDomElement& elem ) {
 		//get the caption and restore all child items!
 		if (elem.hasAttribute("caption"))
 			setText(0, elem.attribute("caption"));
@@ -76,7 +97,7 @@ SubFolder::SubFolder(CFolderBase* parentItem, QDomElement& xml ) : CBookmarkFold
 		QDomElement child = elem.firstChild().toElement();
 		CIndexItemBase* oldItem = 0;
 		while ( !child.isNull() && child.parentNode() == elem ) {
-			CItemBase* i = 0;
+			CIndexItemBase* i = 0;
 			if (child.tagName() == "Folder") {
 				i = new CIndexSubFolder(this, child);
 			}
