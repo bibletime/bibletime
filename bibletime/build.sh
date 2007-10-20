1ef619f804d5dc849d325cb70544c6919a63865e
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2007-10-20 12:11:13 +0000

    let kde4-config be found
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@381 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/build.sh
+++ bibletime/build.sh
@@ -1,6 +1,9 @@
 # This little script creates a build/ directory, enters it
 # and builds BibleTime there.
 
+#TMP FIX; expand path to let kde4-config be found
+PATH="$PATH:/usr/lib/kde4/bin"
+
 if [ ! -d build ]; then mkdir build; fi
 cd build
 VERBOSE=1 cmake ..
