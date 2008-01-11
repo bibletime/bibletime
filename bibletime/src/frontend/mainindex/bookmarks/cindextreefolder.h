928997492531cd32f1830b6a141251b386c2c5fa
eelik <eelik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2008-01-11 11:48:50 +0000

    Docstring change.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@567 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/mainindex/bookmarks/cindextreefolder.h
+++ bibletime/src/frontend/mainindex/bookmarks/cindextreefolder.h
@@ -23,15 +23,17 @@ class CSwordModuleInfo;
 class CMainIndex;
 
 
-/** The base class for all items in the tree. Subclasses for module folders, modules and bookmarks exist.
-  * @author The BibleTime team
-  */
+/**
+ * The toplevel folder for the bookmarks.
+ * @author The BibleTime team
+ */
 class CIndexTreeFolder : public CIndexFolderBase {
 public:
 	CIndexTreeFolder(CBookmarkIndex* mainIndex, const Type type);
 	CIndexTreeFolder(CIndexFolderBase* parentFolder, const Type type);
 	virtual ~CIndexTreeFolder();
 
+	/** */
 	virtual void addGroup(const Type type);
 
 	virtual void addBookmark(CSwordModuleInfo* module, const QString& key, const QString& description);
