33530de0d623217707fce7604a5c44dfae866f24
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2008-01-18 18:22:56 +0000

    ECLIPSE RULES! =)
    Improved config.h handling
    Found mechanism to use for other files like bibletime.desktop.in as well (CONFIGURE_FILE)
    Added boost as requirement to README
    
    
    
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@585 ba6c7691-6512-0410-bf18-b6da21b6447f

---
+++ bibletime/cmake/config.h.cmake
@@ -0,0 +1,16 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2008 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef CONFIG_H_CMAKE_
+#define CONFIG_H_CMAKE_
+
+#cmakedefine BT_VERSION "${BT_VERSION}"
+
+
+#endif /*CONFIG_H_CMAKE_*/
