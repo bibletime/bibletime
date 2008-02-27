3ce400547eded8630b8e886ffb3a490237a7cb46
eelik <eelik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2008-02-27 17:05:13 +0000

    Addition to the previous commit, was left out for some unknown reason.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@646 ba6c7691-6512-0410-bf18-b6da21b6447f

---
+++ bibletime/src/frontend/bookshelfmanager/new/btinstallprogressdialog.h
@@ -0,0 +1,39 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2008 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef BTINSTALLPROGRESSDIALOG_H
+#define BTINSTALLPROGRESSDIALOG_H
+
+#include <QDialog>
+#include <QString>
+
+class QTreeWidget;
+
+
+/**
+
+*/
+class BtInstallProgressDialog : public QDialog
+{
+	Q_OBJECT
+public:
+	BtInstallProgressDialog(QTreeWidget* selectedModulesTreeWidget, QString destination);
+
+	~BtInstallProgressDialog();
+
+public slots:
+	void slotOneItemCompleted(QString);
+	void slotOneItemStopped(QString);
+	void slotStopInstall();
+
+private:
+	QList<QThread*> m_threads;
+};
+
+#endif
