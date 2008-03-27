d74b5693a3f9e18e2e2c65511f2e468fe511bcd5
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2008-03-27 17:35:30 +0000

    Added lemma handling fix by Tom Cornell
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@700 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/backend/filters/bt_osishtml.cpp
+++ bibletime/src/backend/filters/bt_osishtml.cpp
@@ -141,9 +141,7 @@ bool Filters::BT_OSISHTML::handleToken(sword::SWBuf &buf, const char *token, swo
 						val = strchr(attrib, ':');
 						val = (val) ? (val + 1) : attrib;
 
-						if ((*val == 'H') || (*val == 'G')) {
-							attrValue.append(val);
-						}
+						attrValue.append(val);
 					}
 					while (++i < count);
 
