5dd70115279c67318229f72aed1e6083dafd98ce
gholmlund <gholmlund@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-01-20 03:50:38 +0000

    Add one more file to the QtWebKit check in.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@939 ba6c7691-6512-0410-bf18-b6da21b6447f

---
+++ bibletime/src/frontend/display/bthtmljsobject.h
@@ -0,0 +1,55 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2009 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+#ifndef BTHTMLJSOBJECT_H
+#define BTHTMLJSOBJECT_H
+
+#include <QObject>
+#include <QPoint>
+
+class BtHtmlReadDisplay;
+
+class BtHtmlJsObject: public QObject
+{
+	Q_OBJECT
+
+public:
+	BtHtmlJsObject(BtHtmlReadDisplay* display);
+	~BtHtmlJsObject(){}
+	void moveToAnchor(const QString& anchor);
+
+public slots: void mouseMoveEvent(const QString& attributes, const int& x, const int& y, const bool& shiftKey);
+	void mouseDownLeft(const QString& url, const int& X, const int& Y);
+	void mouseDownRight(const QString& url);
+	void timeOutEvent(const QString& attributes);
+
+signals:
+	void startTimer(int time);
+	void mouseMoveAttribute(const QString& attrName, const QString& attrValue);
+	void gotoAnchor(const QString& anchor);
+
+private:
+	int m_int;
+	BtHtmlReadDisplay* m_display;
+
+	struct DNDData 
+	{
+		bool mousePressed;
+		bool isDragging;
+		QPoint startPos;
+		QString url;
+		enum DragType 
+		{
+			Link,
+			Text
+		} dragType;
+	}		m_dndData;
+
+};
+
+#endif
