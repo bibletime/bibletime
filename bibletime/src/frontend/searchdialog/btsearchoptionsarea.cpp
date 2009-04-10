0a6780f8031594df31b82f3112e5ea52e5982dda
eelik <eelik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-04-10 21:32:35 +0000

    Updated the search syntax help text.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@1132 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/searchdialog/btsearchoptionsarea.cpp
+++ bibletime/src/frontend/searchdialog/btsearchoptionsarea.cpp
@@ -337,27 +337,107 @@ void BtSearchOptionsArea::setupRanges() {
 }
 
 void BtSearchOptionsArea::syntaxHelp() {
-	QString syntax = tr(
-			"<p>Enter search terms separated by spaces.  By default the search "
-			"function will return results that match any of the search terms (OR). "
-			"To search for all the terms separate the terms by AND.</p>") + tr(
-			"<p>You can use wildcards: '*' matches any sequence of characters, "
-			"while '?' matches any single character. The use of brackets allows you to "
-			"group your search terms, e.g. '(Jesus OR spirit) AND God'.</p>") + tr(
-			"<p>To search text other than the main text, enter the text type followed "
-			"by \":\", and then the search term.  For example, to search for the Strong's "
-			"number H8077, use 'strong:H8077'.</p>") + tr(
-			"<p>Available text types:<br><table>"
-			"<tr><td>heading:</td><td>searches headings</td></tr>"
-			"<tr><td>footnote:</td><td>searches footnotes</td></tr>"
-			"<tr><td>strong:</td><td>searches Strong's Numbers</td></tr>"
-			"<tr><td>morph:</td><td>searches morphology codes</td></tr></table></p>") + tr(
-			"<p>BibleTime uses the Lucene search engine to perform your searches. "
-			"It has many advanced features, and you can read more about it here: "
-			"<a href=\"http://lucene.apache.org/java/docs/queryparsersyntax.html\">"
-			"http://lucene.apache.org/java/docs/queryparsersyntax.html</a></p>");
 
-	BtTabHtmlDialog* dlg = new BtTabHtmlDialog(tr("Basic Search Syntax Introduction"), 0, this);
+	QString style = QString(
+	"<style type='text/css'>"
+	"h1 a {font-size: medium}"
+	"table {margin-left: 20px}"
+	"td {"
+	"	border-width: 3px 3px 3px 3px;"
+	"	border-style: solid solid solid solid;"
+	"	border-color: white white white white;"
+	"	background-color: #faf0e6;"
+	"}"
+	"p#links {margin-left: 20px}"
+	"</style>");
+	
+	//: Don't translate words inside <> tags!
+	//: Translate 'All words' etc. indentically to the Search dialog options.
+	QString intro = tr(
+	"<p>"
+	"This help is mainly for 'Full syntax' option. 'All words' and 'Some words' options " "have more limited syntax; <a href='#wildcards'>wildcards</a> and <a " "href='#fields'>text fields</a> are supported for them. Some other syntax features " "may give strange or wrong results with All words/Some words."
+	"</p>");
+	
+	QString links = tr(
+	"<p id='links'>"
+	"<A href='#allsome'>Which words to find</A><br />"
+	"<A href='#grouping'>Grouping and order</A><br />"
+	"<A href='#wildcards'>Wildcards (partial words)</A><br />"
+	"<A href='#fields'>Text fields (different parts of text)</A><br/>"
+	"<A href='#lucene'>Other syntax features</A><br/>"
+	"</p>");
+	
+	//: Syntax words (AND, OR...) must not be translated.
+	QString whichwords = tr(
+	"<h1><A name='allsome'>Which words to find</A></h1>"
+	"<p>Search terms are separated by spaces. <strong>AND</strong> (all words), " "<strong>OR</strong> (some words) and <strong>NOT</strong> (not the following word) " "can be added between the words. If none is added explicitly OR is used " "automatically. '<strong>+</strong>word' means the word must be in the results, " "'<strong>-</strong>word' means it must not be in the results.</p>");
+	
+	//: In examples words to be searched for may be translated, but syntax words (AND, OR...) must not be translated.
+	QString whichwordstable = tr(
+	"<p><table><tr>"
+	"<td>jesus AND god</td><td>Finds verses with both 'Jesus' and 'God'</td>"
+	"</tr><tr>"
+	"<td>jesus OR god</td><td>Finds verses with 'Jesus' or 'God' or both</td>"
+	"</tr><tr>"
+	"<td>jesus NOT god</td><td>Finds verses with 'Jesus' but with no 'God'</td>"
+	"</tr><tr>"
+	"<td>+jesus -god</td><td>Finds verses with 'Jesus' but with no 'God'</td>"
+	"</tr></table></p>");
+	
+	QString grouping = tr(
+	"<h1><A name='grouping'>Grouping and order</A></h1>"
+	"<p>Words can be grouped with <strong>parenthesis</strong>. "
+	"Strict word order can be defined with <strong>quotes</strong>.</p>");
+	
+	QString groupingtable = tr(
+	"<p><table><tr>"
+	"<td>(a AND b) OR c</td><td>Finds verses with both 'a' AND 'b', and verses with 'c'</td>"
+	"</tr><tr>"
+	"<td>\"says lord\"</td><td>Finds e.g. '...Isaiah says, \"Lord...' but not '...says the LORD'</td>"
+	"</tr><tr>"
+	"<td>\"says the lord\"</td><td>Finds all verses with 'says the LORD'</td>"
+	"</tr></table></p>");
+	
+	QString wildcards = tr(
+	"<h1><A name='wildcards'>Wildcards (partial words)</A></h1>"
+	"<p>'<strong>*</strong>' matches any sequence of 0 or more characters, while '<strong>?</strong>' matches any single character. A wildcard can not be used in the beginning of a word.</p>");
+	
+	QString wildcardstable = tr(
+	"<p><table><tr>"
+	"<td>a*</td><td>All words beginning with 'a'</td>"
+	"</tr><tr>"
+	"<td>a*a</td><td>'Assyria', 'aroma', 'abba' etc.</td>"
+	"</tr><tr>"
+	"<td>a?</td><td>'at' and 'an'</td>"
+	"</tr><tr>"
+	"<td>a??a</td><td>'abba', 'area', 'Asia' etc.</td>"
+	"</tr></table></p>");
+	
+	QString fields = tr(
+	"<h1><A name='fields'>Text fields (different parts of text)</A></h1>"
+	"<p>Available text fields:<br /><table>"
+	"<tr><td>heading:</td><td>Searches headings</td></tr>"
+	"<tr><td>footnote:</td><td>Searches footnotes</td></tr>"
+	"<tr><td>strong:</td><td>Searches Strong's numbers</td></tr>"
+	"<tr><td>morph:</td><td>Searches morphology codes</td></tr></table></p>");
+	
+	QString fieldstable = tr(
+	"<p>Examples:<br /><table>"
+	"<tr><td>heading:Jesus</td><td>Finds headings with 'Jesus'</td></tr>"
+	"<tr><td>footnote:Jesus AND footnote:said</td><td>Finds footnotes with 'Jesus' and 'said'</td></tr>"
+	"<tr><td>strong:G846</td><td>Finds verses with Strong's Greek number 846</td></tr>"
+	"<tr><td>morph:\"N-NSF\"</td><td>Finds verses with morphology code 'N-NSF'</td>"
+	"</tr></table></p>");
+	
+	QString lucene = tr(
+	"<h1><A name='lucene'>Other syntax features</A></h1>"
+	"<p>BibleTime uses the CLucene search engine. You can read more on the <a href='http://lucene.apache.org/java/1_4_3/queryparsersyntax.html'>lucene syntax web page</a> (in external browser).</p>");
+
+	QString syntax = style + intro + links + whichwords + whichwordstable +
+		grouping + groupingtable + wildcards + wildcardstable +
+		fields + fieldstable + lucene;
+
+	BtTabHtmlDialog* dlg = new BtTabHtmlDialog(tr("Search Syntax Help"), 0, this);
 	dlg->setHtml(syntax);
 	dlg->show();
 }
