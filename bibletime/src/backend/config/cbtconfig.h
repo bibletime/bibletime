05fc371ba9265ae105bd9cb8780655e4f7ddb56a
jotik <jotik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-08-12 20:48:21 +0000

    Minor code restructuring fixes in cbtconfig.h. Nothing refactored.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@1404 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/backend/config/cbtconfig.h
+++ bibletime/src/backend/config/cbtconfig.h
@@ -10,26 +10,25 @@
 #ifndef CBTCONFIG_H
 #define CBTCONFIG_H
 
-#include "backend/managers/cswordbackend.h"
-
-//Qt includes
 #include <QString>
 #include <QFont>
 #include <QList>
 #include <QMap>
+#include "backend/managers/cswordbackend.h"
 
-//Forward declarations
 class QSettings;
 class BtActionCollection;
-//class CLanguageMgr::Language;
 
 /**
  * This class is the interface to the config object of BibleTime
  * @author The BibleTime team
  */
 class CBTConfig {
+    friend class BibleTimeTest;
+
 public:
 	typedef QMap<QString, QString> StringMap;
+    typedef std::pair<bool, QFont> FontSettingsPair;
 
 	enum strings {
 		bibletimeVersion,
@@ -135,42 +134,42 @@ public:
 	enum stringMaps {
 		searchScopes
 	};
-	typedef std::pair<bool, QFont> FontSettingsPair;
 
-	static QString get( const CBTConfig::strings );
-	static CSwordModuleInfo* get( const CBTConfig::modules );
-	static bool get( const CBTConfig::bools );
-	static int get( const CBTConfig::ints );
-	static QList<int> get( const CBTConfig::intLists );
-	static QStringList get( const CBTConfig::stringLists );
-	static CBTConfig::StringMap get( const CBTConfig::stringMaps );
+public:
+    static QString get( const strings );
+    static CSwordModuleInfo* get( const modules );
+    static bool get( const bools );
+    static int get( const ints );
+    static QList<int> get( const intLists );
+    static QStringList get( const stringLists );
+    static StringMap get( const stringMaps );
 
 	static FontSettingsPair get( const CLanguageMgr::Language* const );
 
-	static QString getDefault( const CBTConfig::strings );
-	static QString getDefault( const CBTConfig::modules );
-	static bool getDefault( const CBTConfig::bools );
-	static int getDefault( const CBTConfig::ints );
-	static QList<int> getDefault( const CBTConfig::intLists );
-	static QStringList getDefault( const CBTConfig::stringLists );
-	static CBTConfig::StringMap getDefault( const CBTConfig::stringMaps );
+    static QString getDefault( const strings );
+    static QString getDefault( const modules );
+    static bool getDefault( const bools );
+    static int getDefault( const ints );
+    static QList<int> getDefault( const intLists );
+    static QStringList getDefault( const stringLists );
+    static StringMap getDefault( const stringMaps );
 	static QFont& getDefault( const CLanguageMgr::Language* const );
 
-	static void set( const CBTConfig::strings,  const QString value );
-	static void set( const CBTConfig::modules, CSwordModuleInfo* const module );
-	static void set( const CBTConfig::modules, const QString& moduleName );
-	static void set( const CBTConfig::bools,   const bool value );
-	static void set( const CBTConfig::ints,   const int value );
-	static void set( const CBTConfig::intLists, const QList<int> value );
-	static void set( const CBTConfig::stringLists, const QStringList value);
-	static void set( const CBTConfig::stringMaps, const CBTConfig::StringMap value);
+    static void set( const strings,  const QString value );
+    static void set( const modules, CSwordModuleInfo* const module );
+    static void set( const modules, const QString& moduleName );
+    static void set( const bools,   const bool value );
+    static void set( const ints,   const int value );
+    static void set( const intLists, const QList<int> value );
+    static void set( const stringLists, const QStringList value);
+    static void set( const stringMaps, const StringMap value);
 	static void set( const CLanguageMgr::Language* const language, const FontSettingsPair& fontSettings );
 
 	static CSwordBackend::FilterOptions getFilterOptionDefaults();
 	static CSwordBackend::DisplayOptions getDisplayOptionDefaults();
 
-	static void setupAccelSettings(const CBTConfig::keys type, BtActionCollection* const actionCollection);
-	static void saveAccelSettings(const CBTConfig::keys type, BtActionCollection* const actionCollection);
+    static void setupAccelSettings(const keys type, BtActionCollection* const actionCollection);
+    static void saveAccelSettings(const keys type, BtActionCollection* const actionCollection);
 
 	static QString getModuleEncryptionKey( const QString& name );
 	static void setModuleEncryptionKey( const QString& name, const QString& key );
@@ -182,26 +181,24 @@ public:
 	static QSettings* getConfig();
 
 private:
-	friend class BibleTimeTest;
+    typedef QMap<const CLanguageMgr::Language*, FontSettingsPair> FontCache;
 
-	static QString getKey( const CBTConfig::strings );
-	static QString getKey( const CBTConfig::modules );
-	static QString getKey( const CBTConfig::bools );
-	static QString getKey( const CBTConfig::ints );
-	static QString getKey( const CBTConfig::intLists );
-	static QString getKey( const CBTConfig::stringLists );
-	static QString getKey( const CBTConfig::stringMaps );
+    static QString getKey( const strings );
+    static QString getKey( const modules );
+    static QString getKey( const bools );
+    static QString getKey( const ints );
+    static QString getKey( const intLists );
+    static QString getKey( const stringLists );
+    static QString getKey( const stringMaps );
 	static QString getKey( const CLanguageMgr::Language* const );
 
 	static QString IntListToString( const QList<int> );
 	static QList<int> StringToIntList( const QString );
 
+private:
 	//static caches
 	static QFont* m_defaultFont;
-
-	typedef QMap<const CLanguageMgr::Language*, CBTConfig::FontSettingsPair> FontCache;
-	static QMap<const CLanguageMgr::Language*, CBTConfig::FontSettingsPair> fontConfigMap;
+    static FontCache fontConfigMap;
 };
-
 
 #endif
