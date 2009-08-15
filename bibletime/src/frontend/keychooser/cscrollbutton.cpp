5a820c8e371a5c52590ab049f9da2b138a4371a5
jotik <jotik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-08-15 12:10:37 +0000

    Rewrote CScrollButton, because of a bug that made the cursor vanish in the whole application even after releasing the mouse button. This bug was caused by the released() signal not always being emitted. Instead the widget now grabs the mouse. I also improved the documentation and optimized the code.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@1414 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/keychooser/cscrollbutton.cpp
+++ bibletime/src/frontend/keychooser/cscrollbutton.cpp
@@ -2,20 +2,14 @@
 *
 * This file is part of BibleTime's source code, http://www.bibletime.info/.
 *
-* Copyright 1999-2008 by the BibleTime developers.
+* Copyright 1999-2009 by the BibleTime developers.
 * The BibleTime source code is licensed under the GNU General Public License version 2.0.
 *
 **********/
 
-
-
 #include "cscrollbutton.h"
 
-
-#include <stdlib.h>
-#include <math.h>
-
-//Qt includes
+#include <cmath>
 #include <QEvent>
 #include <QApplication>
 #include <QCursor>
@@ -23,63 +17,67 @@
 #include <QMouseEvent>
 #include <QWheelEvent>
 
-CScrollButton::CScrollButton(QWidget *parent) : QToolButton(parent) {
-	setFocusPolicy(Qt::WheelFocus);
-	setCursor(Qt::SplitVCursor );
-
-	m_isLocked = false;
-	connect(this, SIGNAL(pressed() ), SLOT(was_pressed() ));
-	connect(this, SIGNAL(released()), SLOT(was_released()));
+CScrollButton::CScrollButton(QWidget *parent)
+    : QToolButton(parent), m_isLocked(false)
+{
+    setFocusPolicy(Qt::WheelFocus);
+    setCursor(Qt::SplitVCursor);
 }
 
-bool CScrollButton::isLocked( ) const {
-	return m_isLocked;
+CScrollButton::~CScrollButton() {
+    // Intentionally empty
 }
 
-void CScrollButton::was_pressed( ) {
-	QApplication::setOverrideCursor(Qt::BlankCursor);
-	m_isLocked = true;
-	lock_Point = get_lock_Point();
-
-	emit lock()
-		;
+void CScrollButton::mousePressEvent(QMouseEvent *e) {
+    if (m_isLocked) return;
+    if (e->button() != Qt::LeftButton) return;
+    m_isLocked = true;
+    grabMouse(Qt::BlankCursor);
+    emit lock();
 }
 
-void CScrollButton::was_released( ) {
-	QApplication::restoreOverrideCursor();
-	m_isLocked = false;
-
-	emit unlock();
+void CScrollButton::mouseReleaseEvent(QMouseEvent *e) {
+    if (!m_isLocked) return;
+    if (e->button() != Qt::LeftButton) return;
+    m_isLocked = false;
+    releaseMouse();
+    emit unlock();
 }
 
-const QPoint CScrollButton::get_lock_Point() const {
-	return mapToGlobal( QPoint( width()/2, height()/2 ) );
-}
+void CScrollButton::mouseMoveEvent(QMouseEvent *e) {
+    if (m_isLocked) {
+        // Recalculate the center of the widget (might change during grab):
+        QPoint center(mapToGlobal(QPoint(width() / 2, height() / 2)));
 
-void CScrollButton::mouseMoveEvent( QMouseEvent* e ) {
-	if (m_isLocked) {
-		int vchange = (QCursor::pos().y() - lock_Point.y());
+        // Calculate movement change:
+        int vchange = (e->globalY() - center.y());
 
-		if (abs(vchange) < 10) {
-			vchange = (int)((vchange>0 ? 1 : -1) * pow(abs(vchange), 0.3));
-		}
-		else if (abs(vchange) < 30) {
-			vchange = (int)((vchange>0 ? 1 : -1) * pow(abs(vchange), 0.6));
-		}
-		else if (abs(vchange) < 40) {
-			vchange = (int)((vchange>0 ? 1 : -1) * pow(abs(vchange), 1.2));
-		}
-		else {
-			vchange = (int)((vchange>0 ? 1 : -1) * pow(abs(vchange), 2.0));
-		}
+        if (vchange != 0) {
+            // Calculate the real change we are going to emit:
+            int avchange(vchange >= 0 ? vchange : -vchange);
+            if (avchange < 10) {
+                avchange = (int) pow(avchange, 0.3);
+            } else if (avchange < 30) {
+                avchange = (int) pow(avchange, 0.6);
+            } else if (avchange < 40) {
+                avchange = (int) pow(avchange, 1.2);
+            } else {
+                avchange = (int) pow(avchange, 2.0);
+            }
 
-		if (vchange) { //not emit 0
-			emit change_requested( vchange );
-		}
+            // Emit the change request signal only when necessary:
+            if (avchange != 0) {
+                if (vchange > 0) {
+                    emit change_requested(avchange);
+                } else if (vchange < 0) {
+                    emit change_requested(-avchange);
+                }
+            }
+        }
 
-		QCursor::setPos( lock_Point );
-	}
-	else {
-		QToolButton::mouseMoveEvent(e);
-	}
+        // Move the mouse cursor to the center of this widget:
+        QCursor::setPos(center);
+    } else {
+        QToolButton::mouseMoveEvent(e);
+    }
 }
