61e8dd31999b996440546982799fe86f0d14b82a
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-10-10 17:01:59 +0000

    Move logic from script to cmake file.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@1570 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/cmake/BTDeveloper.cmake
+++ bibletime/cmake/BTDeveloper.cmake
@@ -1,23 +1,16 @@
+FILE(GLOB_RECURSE ORIG_SOURCE_FILES
+    RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}/src/"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/*.h"
+)
+FOREACH(FILE ${ORIG_SOURCE_FILES})
+	SET(REFORMAT_FILES "${REFORMAT_FILES} ${FILE}")
+ENDFOREACH(FILE ${ORIG_SOURCE_FILES})
 
-#
-# There was an issue using cmake's default mechanisms (maybe a limit to the parameters
-# that may be passed to a command), so I used a shellsript instead.
-#
-
-#FILE(GLOB_RECURSE ORIG_SOURCE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/src" "*.cpp")
-#FOREACH(FILE ${ORIG_SOURCE_FILES})
-#	SET(REFORMAT_FILES "${REFORMAT_FILES} ${FILE}")
-#ENDFOREACH(FILE ${ORIG_SOURCE_FILES})
-#
-#FILE(GLOB_RECURSE ORIG_SOURCE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/src" "*.h")
-#FOREACH(FILE ${ORIG_SOURCE_FILES})
-#	SET(REFORMAT_FILES "${REFORMAT_FILES} ${FILE}")
-#ENDFOREACH(FILE ${ORIG_SOURCE_FILES})
-
-#SET(ASTYLE_OPTIONS "--indent=spaces=4 --brackets=attach --indent-classes --indent-switches --brackets=break-closing --pad=oper --suffix=none --options=none")
+SET(ASTYLE_OPTIONS "--indent=spaces=4 --brackets=attach --indent-classes --indent-switches --brackets=break-closing --pad=oper --suffix=none --options=none")
 
 ADD_CUSTOM_TARGET("reformat_codebase"
-#	COMMAND astyle ${ASTYLE_OPTIONS}  ${REFORMAT_FILES}
-	COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/cmake/scripts/reformat_codebase.sh"
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+	COMMAND "sh" "-c" "astyle ${ASTYLE_OPTIONS}  ${REFORMAT_FILES}"
+	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/src/"
+	VERBATIM
 )
