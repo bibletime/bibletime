15be6716e25a9f7668574ac333baa9beccb8ddeb
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-11-14 20:01:19 +0000

    Fixed a bug where searching for more than one Strongs number from the read window did not work correctly.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@1798 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/displaywindow/creadwindow.cpp
+++ bibletime/src/frontend/displaywindow/creadwindow.cpp
@@ -193,7 +193,9 @@ void CReadWindow::openSearchStrongsDialog() {
     QString searchText = QString::null;
 
     if (displayWidget()->getCurrentNodeInfo()[CDisplay::Lemma] != QString::null) {
-        searchText.append("strong:").append(displayWidget()->getCurrentNodeInfo() [CDisplay::Lemma]);
+    	Q_FOREACH(QString strongNumber, displayWidget()->getCurrentNodeInfo()[CDisplay::Lemma].split("|")) {
+    		searchText.append("strong:").append( strongNumber ).append(" ");
+    	}
     }
 
     Search::CSearchDialog::openDialog( modules(), searchText, 0 );
