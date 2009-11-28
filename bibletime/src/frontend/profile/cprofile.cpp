8e6c53537d19970ecee4d5fd15a75d545824f612
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-11-28 16:07:29 +0000

    Reformat codebase according to our agreed guidelines available at http://devel.bibletime.info/wiki/Programming_Guidelines.
    Developers, please verify that you set your editors to 4 spaces per indentation level. Tabs should not be used.
    
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@1945 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/profile/cprofile.cpp
+++ bibletime/src/frontend/profile/cprofile.cpp
@@ -97,9 +97,9 @@ QList<CProfileWindow*> CProfile::load() {
                 if (mdi_element.hasAttribute("ArrangementMode")) {
                     this->setMDIArrangementMode((CMDIArea::MDIArrangementMode)mdi_element.attribute("ArrangementMode").toInt());
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
     m_profileWindows.clear();
     QDomElement elem = document.firstChild().toElement();
@@ -181,18 +181,18 @@ QList<CProfileWindow*> CProfile::load() {
         }
         elem = elem.nextSibling().toElement();
     }
-    
+
     // Are any windows maximized?
     bool maximized = false;
     for (int i = 0; i < m_profileWindows.count(); i++) {
         if (m_profileWindows.at(i)->maximized())
-          maximized = true;
+            maximized = true;
     }
     // Set all windows the same for maximized
     for (int i = 0; i < m_profileWindows.count(); i++) {
         m_profileWindows.at(i)->setMaximized(maximized);
     }
-    
+
     return m_profileWindows;
 }
 
