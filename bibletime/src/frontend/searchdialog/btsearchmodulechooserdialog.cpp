ed5f8ca93d960e2bf19e4ead1e15ffbb75aa1dd0
jotik <jotik@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-12-21 20:10:09 +0000

    Fixed headed comment.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@2084 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/searchdialog/btsearchmodulechooserdialog.cpp
+++ bibletime/src/frontend/searchdialog/btsearchmodulechooserdialog.cpp
@@ -6,7 +6,7 @@
 *
 * Copyright 1999-2009 by the BibleTime developers.
 * The BibleTime source code is licensed under the GNU General Public License
-b* version 2.0.
+* version 2.0.
 *
 **********/
 
