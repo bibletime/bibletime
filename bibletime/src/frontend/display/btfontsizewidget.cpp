67f0b71c6dbac02ebd92837fa318e7fee3cc5f1e
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2010-01-26 17:41:05 +0000

    Corrected license header.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@2165 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/src/frontend/display/btfontsizewidget.cpp
+++ bibletime/src/frontend/display/btfontsizewidget.cpp
@@ -1,9 +1,9 @@
 /*********
 *
-* This file is part of BtFontSizeWidget's source code, http://www.bibletime.info/.
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
 *
-* Copyright 1999-2008 by the BtFontSizeWidget developers.
-* The BtFontSizeWidget source code is licensed under the GNU General Public License version 2.0.
+* Copyright 1999-2010 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
 *
 **********/
 
