38dcde8d291ede6f7a088038029f258c4ac163c6
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2010-06-09 19:11:41 +0000

    Some progress on mac .dmg generation. The build still fails, but it seems clearer now which steps need to be taken.
    git-svn-id: https://bibletime.svn.sf.net/svnroot/bibletime/trunk@2377 ba6c7691-6512-0410-bf18-b6da21b6447f

--- bibletime/cmake/BTBinaryPackageMacOS.cmake
+++ bibletime/cmake/BTBinaryPackageMacOS.cmake
@@ -1,39 +1,4 @@
 IF(APPLE)
-    INCLUDE(${QT_USE_FILE})
-    SET(QT_FRAMEWORKS_USED
-        "QtWebKit"
-        "QtGui"
-        "QtXml"
-        "QtTest"
-        "QtNetwork"
-        "QtCore"
-        "QtSvg"
-    )
-    FOREACH(QT_FRAME ${QT_FRAMEWORKS_USED})
-        INSTALL(DIRECTORY
-            "${QT_LIBRARY_DIR}/${QT_FRAME}.framework"
-            DESTINATION "${BT_DESTINATION}/../Library/Frameworks/"
-        )
-    ENDFOREACH(QT_FRAME ${QT_FRAMEWORKS_USED})
-
-    # Other libraries I need
-    INSTALL(FILES "${CLUCENE_LIBRARY}"
-        "${CLUCENE_LIBRARY_DIR}/libclucene.0.0.0.dylib"
-		"${CLUCENE_LIBRARY_DIR}/libclucene.0.dylib"
-		"/opt/local/lib/libcurl.4.dylib"
-   	    "/opt/local/lib/libssl.0.9.8.dylib"
-       	"/opt/local/lib/libcrypto.0.9.8.dylib"
-#        "/opt/local/lib/libicui18n.40.dylib"
-#	       	"/opt/local/lib/libicui18n.40.0.dylib"
-#		"/opt/local/lib/libicuuc.40.dylib"
-#	       	"/opt/local/lib/libicuuc.40.0.dylib"
-#	       	"/opt/local/lib/libicuio.40.dylib"
-#	       	"/opt/local/lib/libicuio.40.0.dylib"
-#		"/opt/local/lib/libicudata.40.dylib"
-#	       	"/opt/local/lib/libicudata.40.0.dylib"
-		"/opt/local/lib/libpng12.0.dylib"
-          DESTINATION "${BT_DESTINATION}/../Library")
-
     # Qt Plugins
     INSTALL(DIRECTORY "${QT_PLUGINS_DIR}/iconengines" "${QT_PLUGINS_DIR}/imageformats" DESTINATION "${BT_DESTINATION}/../../plugins")
     # A file to execute that might get rid of the above mess
@@ -46,6 +11,11 @@ IF(APPLE)
     SET(CPACK_GENERATOR "Bundle")
 
     SET(CPACK_PACKAGE_EXECUTABLES "bibletime" "BibleTime")
+
+    INSTALL(CODE "
+        INCLUDE(BundleUtilities)
+        FIXUP_BUNDLE(\"\${CMAKE_INSTALL_PREFIX}/bin/BibleTime\" \"\" \"\")
+        " COMPONENT Runtime)
 
     INCLUDE(CPack)
 ENDIF(APPLE)
