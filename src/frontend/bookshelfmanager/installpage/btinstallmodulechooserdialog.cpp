bibletime/bibletime

commit dfd3e300c8b3543d08f786e19ab8f1673d090d54

Replaced all tabs with 4 spaces in source code.

src/frontend/bookshelfmanager/installpage/btinstallmodulechooserdialog.cpp
@@ -54,7 +54,7 @@ void BtInstallModuleChooserDialog::retranslateUi() {
     setWindowTitle(tr("Install/Update works?"));
     util::tool::initExplanationLabel(
             label(), QString::null,
-            tr("Do you really want to install these works?") + "<br/><br/><small>" +
+            tr("Do you really want to install these works?") + "<br/><br/><small>"
             tr("Only one version of a work can be installed at the same time. Select only "
                "one if there are items marked with red.") + "</small>");
 }

commit bce6809c261353732f8163d2b7a503a7ca3a8475

Fixed compile error caused by recent dfd3e300c8b3543d08f786e19ab8f1673d090d54.

src/frontend/bookshelfmanager/installpage/btinstallmodulechooserdialog.cpp
@@ -54,7 +54,7 @@ void BtInstallModuleChooserDialog::retranslateUi() {
     setWindowTitle(tr("Install/Update works?"));
     util::tool::initExplanationLabel(
             label(), QString::null,
-            tr("Do you really want to install these works?") + "<br/><br/><small>"
+            tr("Do you really want to install these works?") + "<br/><br/><small>" +
             tr("Only one version of a work can be installed at the same time. Select only "
                "one if there are items marked with red.") + "</small>");
 }