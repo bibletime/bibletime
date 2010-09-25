bibletime/bibletime

commit e0d156632f6fde4ef291573de26242dca415f87f

Implement the Tip Of The Day dialog.

src/bibletime_init.cpp
@@ -258,6 +258,13 @@ void BibleTime::insertKeyboardActions( BtActionCollection* const a ) {
     a->addAction("aboutBibleTime", action);
 
     action = new QAction(a);
+    action->setText(tr("&Tip of the day..."));
+    action->setIcon(DU::getIcon(CResMgr::mainMenu::help::tipOfTheDay::icon));
+    action->setShortcut(QKeySequence(CResMgr::mainMenu::help::tipOfTheDay::accel));
+    action->setToolTip(tr("Tips about the BibleTime program"));
+    a->addAction("tipOfTheDay", action);
+
+    action = new QAction(a);
     a->addAction("showToolbarsInTextWindows", action);
 
     action = new QAction(a);
@@ -497,6 +504,10 @@ void BibleTime::initActions() {
     connect(m_aboutBibleTimeAction,  SIGNAL(triggered()),
             this,                    SLOT(slotOpenAboutDialog()) );
 
+    m_tipOfTheDayAction = m_actionCollection->action("tipOfTheDay");
+    connect(m_tipOfTheDayAction,  SIGNAL(triggered()),
+            this,                    SLOT(slotOpenTipDialog()) );
+
     #ifdef BT_DEBUG
     m_debugWidgetAction = new QAction(this);
     m_debugWidgetAction->setCheckable(true);
@@ -591,6 +602,7 @@ void BibleTime::initMenubar() {
     m_helpMenu = new QMenu(this);
     m_helpMenu->addAction(m_openHandbookAction);
     m_helpMenu->addAction(m_bibleStudyHowtoAction);
+    m_helpMenu->addAction(m_tipOfTheDayAction);
     m_helpMenu->addSeparator();
     m_helpMenu->addAction(m_aboutBibleTimeAction);
     #ifdef BT_DEBUG

src/frontend/tips/bttipdialog.cpp
@@ -1,0 +1,165 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2010 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#include "bttipdialog.h"
+
+#include "backend/config/cbtconfig.h"
+#include "util/cresmgr.h"
+#include "util/directory.h"
+
+#include <QCheckBox>
+#include <QDialogButtonBox>
+#include <QAbstractButton>
+#include <QWebView>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QDesktopServices>
+
+BtTipDialog::BtTipDialog(QWidget *parent, Qt::WindowFlags wflags)
+        : QDialog(parent, wflags), m_buttonBox(0), m_tipView(0), m_showTipsCheckBox(0)
+{
+    Qt::WindowFlags flags = windowFlags();
+    setWindowFlags(flags & ~Qt::WindowContextHelpButtonHint);
+
+    setWindowTitle(tr("Tip Of The Day"));
+    resize(450, 220);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+    m_tipView = new QWebView;
+    m_tipView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    mainLayout->addWidget(m_tipView);
+
+    QHBoxLayout* hLayout = new QHBoxLayout;
+
+    m_showTipsCheckBox = new QCheckBox;
+    m_showTipsCheckBox->setText(tr("Show tips at startup"));
+    bool showTips = CBTConfig::get(CBTConfig::showTipAtStartup);
+    m_showTipsCheckBox->setChecked(showTips);
+    hLayout->addWidget(m_showTipsCheckBox);
+
+    m_buttonBox = new QDialogButtonBox(QDialogButtonBox::Close, Qt::Horizontal, this);
+    m_buttonBox->addButton(tr("Next Tip"), QDialogButtonBox::ActionRole);
+    hLayout->addWidget(m_buttonBox);
+
+    mainLayout->addLayout(hLayout);
+    setLayout(mainLayout);
+
+    bool ok;
+    ok = connect(m_showTipsCheckBox, SIGNAL(stateChanged(int)), this, SLOT(startupBoxChanged(int)));
+    Q_ASSERT(ok);
+
+    ok = connect(m_buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    Q_ASSERT(ok);
+
+    ok = connect(m_buttonBox, SIGNAL(clicked(QAbstractButton*)), this, SLOT(nextTip(QAbstractButton*)));
+    Q_ASSERT(ok);
+
+    m_tipView->page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);
+    ok = connect(m_tipView->page(), SIGNAL(linkClicked(const QUrl&)), this, SLOT(linkClicked(const QUrl&)));
+    Q_ASSERT(ok);
+
+    m_tipNumber = CBTConfig::get(CBTConfig::tipNumber);
+    create_tips();
+    displayTip();
+}
+
+BtTipDialog::~BtTipDialog() {
+    // Intentionally empty
+}
+
+void BtTipDialog::linkClicked(const QUrl& url) {
+    QDesktopServices::openUrl(url);
+}
+
+void BtTipDialog::nextTip(QAbstractButton* button) {
+    QString text = button->text();
+    if (text == tr("Next Tip"))
+    {
+        m_tipNumber++;
+        if (m_tipNumber >= m_tips.count())
+            m_tipNumber = 0;
+        CBTConfig::set(CBTConfig::tipNumber, m_tipNumber);
+        displayTip();
+    }
+}
+
+void BtTipDialog::startupBoxChanged(int value) {
+    bool tipAtStartup = m_showTipsCheckBox->isChecked();
+    CBTConfig::set(CBTConfig::showTipAtStartup, tipAtStartup);
+}
+
+static QString vertical_align(const QString& text)
+{
+    return "<table height=\"100%\"><tr><td style=\"vertical-align:middle\" height=\"100%\">" + text + "</td></tr></table>";
+}
+
+static QString make_style(QWidget* widget)
+{
+    return "<style type=\"text/css\">"\
+     "body{}"\
+    "h3{font-weight:bold;text-align:center}"\
+    "a{text-decoration:underline}"\
+    "a:link{color:" + widget->palette().color(QPalette::Link).name() + "}"\
+    "a:visited{color:" + widget->palette().color(QPalette::LinkVisited).name() + "}"\
+    "</style>";
+}
+
+static QString make_html(QWidget* widget, const QString& text)
+{
+    QString vText = vertical_align(text);
+    return "<html><head>" + make_style(widget) + "</head><body>" + vText + "</body></html>";
+}
+
+static QString make_icon(const QString& icon)
+{
+    namespace DU = util::directory;
+    QString fileName = DU::getIconDir().filePath(icon);
+    QUrl url = QUrl::fromLocalFile(fileName);
+    QString fileUrl = url.toString();
+    return "<img src=\"" + fileUrl + "\" width=\"32\" />";
+}
+
+void BtTipDialog::displayTip() {
+    QString html = make_html(this, m_tips[m_tipNumber]);
+    m_tipView->setHtml(html);
+}
+
+/********************************** Tips *************************************************/
+
+void BtTipDialog::create_tips() {
+
+    QString icon = make_icon(CResMgr::modules::bible::icon_add);
+    m_tips << tr("To add multiple Bible works in parallel in your active Bible window"
+        " select this icon and choose another Bible work.")
+        + "<br><center>" + icon + "</center>";
+
+    icon = make_icon(CResMgr::modules::commentary::icon_add);
+    m_tips << tr("To add multiple commentary works in parallel in your active commentary window"
+        " select this icon and choose another commentary work.")
+        + "<br><center>" + icon + "</center>";
+
+    m_tips << tr("To learn more about the BibleTime project please go to our web site.")
+        + "<br><center><a href=\"http://www.bibletime.info\">www.bibletime.info</a></center>";
+
+    icon = make_icon(CResMgr::displaywindows::commentaryWindow::syncWindow::icon);
+    m_tips << tr("To synchronize a commentary window with the active Bible window, activate the"
+        " commentary window and select this icon.")
+        + "<br><center>" + icon + "</center><br>"
+        + tr("Select the icon again to stop the synchronization.");
+
+    m_tips << tr("To create a bookmark drag any verse reference from a Bible or commentary work"
+        " into the Bookmarks window. An arrow will indicate the position that the bookmark will"
+        " go when you release the cursor. Other works will have a reference in the upper left"
+       " corner that can be used to create a bookmark.");
+ 
+    m_tips << tr("To change a bookmark title or description, right click on the bookmark"
+        " and select \"Edit Bookmark...\". After finishing the edit the description can be"
+        " seen by hovering over the bookmark.");
+}

cmake/BTSourceFileList.cmake
@@ -238,6 +238,13 @@ SET(bibletime_SRC_FRONTEND_PROFILE
 
 SOURCE_GROUP("src\\frontend\\profile" FILES ${bibletime_SRC_FRONTEND_PROFILE})
 
+SET(bibletime_SRC_FRONTEND_TIPDIALOG
+    # Tip dialog:
+    src/frontend/tips/bttipdialog.cpp
+)
+
+SOURCE_GROUP("src\\frontend\\tips" FILES ${bibletime_SRC_FRONTEND_TIPDIALOG})
+
 SET(bibletime_SRC_FRONTEND_KEYCHOOSER
     # Frontend keychooser widgets:
     src/frontend/keychooser/bthistory.cpp
@@ -403,6 +410,7 @@ SET(bibletime_MOCABLE_HEADERS
     src/frontend/settingsdialogs/btlanguagesettings.h
     src/frontend/settingsdialogs/clistwidget.h
     src/frontend/settingsdialogs/cswordsettings.h
+    src/frontend/tips/bttipdialog.h
     src/util/btsignal.h
 )
 
@@ -426,6 +434,7 @@ SET(bibletime_SOURCES
     ${bibletime_SRC_FRONTEND_SEARCHDIALOG}
     ${bibletime_SRC_FRONTEND_SEARCHDIALOG_ANALYSIS}
     ${bibletime_SRC_FRONTEND_SETTINGSDIALOG}
+    ${bibletime_SRC_FRONTEND_TIPDIALOG}
     ${bibletime_SRC_FRONTEND_PROFILE}
     ${bibletime_SRC_FRONTEND_KEYCHOOSER}
     ${bibletime_SRC_FRONTEND_KEYCHOOSER_VERSEKEYCHOOSER}

pics/icons/light_bulb.svg
@@ -1,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg width="103px" height="147px">
+  <defs>
+    <linearGradient id="ThreadsBackgroundGrad">
+      <stop offset="0%" stop-color="#bbb" />
+      <stop offset="100%" stop-color="#666" />
+    </linearGradient>
+    <linearGradient id="MedThreads1Grad" y1="70%" y2="100%">
+      <stop offset="0%" stop-color="#aaa" />
+      <stop offset="100%" stop-color="#333" />
+    </linearGradient>
+    <linearGradient id="DarkThreadsGrad" y1="70%" y2="100%">
+      <stop offset="0%" stop-color="#555" />
+      <stop offset="100%" stop-color="#888" />
+    </linearGradient>
+    <linearGradient id="MedThreads2Grad" y1="70%" y2="100%">
+      <stop offset="0%" stop-color="#aaa" />
+      <stop offset="100%" stop-color="#bbb" />
+    </linearGradient>
+    <linearGradient id="LightThreadsGrad" y1="70%" y2="100%">
+      <stop offset="0%" stop-color="#fff" />
+      <stop offset="100%" stop-color="#444" />
+    </linearGradient>
+    <linearGradient id="EndcapGrad" y1="100%" y2="50%">
+      <stop offset="0%" stop-color="#bbb" />
+      <stop offset="100%" stop-color="#555" />
+    </linearGradient>
+    <radialGradient id="BulbGrad" gradientUnits="userSpaceOnUse" cx="100" cy="100" r="60">
+      <stop offset="0%" stop-color="#fff" />
+      <stop offset="100%" stop-color="#ff6" />
+    </radialGradient>
+  </defs>
+  <g transform="translate(-48.5,-45.34937)">
+    <path id="Bulb" style="fill: url(#BulbGrad)" d="M100,100 m-43.30127,21.69873 a50,50 0 1,1 86.60254,0 c-6,12 -23.30127,18.30127 -23.30127,33.30127 a50,50 0 0,1 -40,0 c0,-15 -17.30127,-21.30127 -23.30127,-33.30127 z" />
+    <path id="Endcap" style="fill: url(#EndcapGrad)" d="M100,100 m-20,82 a50,50 0 0,0 40,-2 l0,3 -10,8 -20,0 -10,-8 z" />
+    <path id="ThreadsBackground" style="fill: url(#ThreadsBackgroundGrad)" d="M100,100 m-20,55 a50,50 0 0,0 40,-2 l0,27 a-50,50 0 0,1 -40,2 z" />
+    <g style="fill: none; stroke-width: 2.25px">
+      <g id="MedThreads1" style="stroke: url(#MedThreads1Grad)">
+        <path id="ThreadsBackground1" d="M100,100 m-20,53 a50,50 0 0,0 40,0" />
+        <path id="ThreadsBackground2" d="M100,100 m-20,82 a50,50 0 0,0 40,0" />
+        <path id="MedThread1" d="M100,100 m-20,61.75 a50,50 0 0,0 40,-2" />
+        <path id="MedThread2" d="M100,100 m-20,70.75 a50,50 0 0,0 40,-2" />
+        <path id="MedThread3" d="M100,100 m-20,79.75 a50,50 0 0,0 40,-2" />
+      </g>
+      <g id="DarkThreads" style="stroke: url(#DarkThreadsGrad)">
+        <path id="DarkThread1" d="M100,100 m-20,55 a50,50 0 0,0 40,-2" />
+        <path id="DarkThread2" d="M100,100 m-20,64 a50,50 0 0,0 40,-2" />
+        <path id="DarkThread3" d="M100,100 m-20,73 a50,50 0 0,0 40,-2" />
+        <path id="DarkThread4" d="M100,100 m-20,82 a50,50 0 0,0 40,-2" />
+      </g>
+      <g id="MedThreads2" style="stroke: url(#MedThreads2Grad)">
+        <path id="MedThread4" d="M100,100 m-20,57.25 a50,50 0 0,0 40,-2" />
+        <path id="MedThread5" d="M100,100 m-20,66.25 a50,50 0 0,0 40,-2" />
+        <path id="MedThread6" d="M100,100 m-20,75.25 a50,50 0 0,0 40,-2" />
+      </g>
+      <g id="LightThreads" style="stroke: url(#LightThreadsGrad)">
+        <path id="LightThread1" d="M100,100 m-20,59.5 a50,50 0 0,0 40,-2" />
+        <path id="LightThread2" d="M100,100 m-20,68.5 a50,50 0 0,0 40,-2" />
+        <path id="LightThread3" d="M100,100 m-20,77.5 a50,50 0 0,0 40,-2" />
+      </g>
+    </g>
+    <path id="Outline" style="fill: none; stroke: #000; stroke-width: 1.5px; stroke-linecap:butt; stroke-linejoin: round" d="M100,100 m-43.30127,21.69873 a50,50 0 1,1 86.60254,0 c-6,12 -23.30127,18.30127 -23.30127,33.30127 l0,28 -10,8 -20,0 -10,-8 0,-28 c0,-15 -17.30127,-21.30127 -23.30127,-33.30127 z" />
+  </g>
+</svg>

src/backend/config/cbtconfig.cpp
@@ -127,6 +127,8 @@ QString getKey(const bools ID) {
             return "showTextWindowToolButtons";
         case showToolbarsInEachWindow:
             return "showToolbarsInEachWindow";
+        case showTipAtStartup:
+            return "showTipAtStartup";
     }
     Q_ASSERT(false);
     return QString::null;
@@ -194,6 +196,8 @@ QString getKey(const ints ID) {
             return "configDialogHeight";
         case configDialogWidth:
             return "configDialogWidth";
+        case tipNumber:
+            return "tipNumber";
     }
     Q_ASSERT(false);
     return QString::null;
@@ -359,6 +363,8 @@ bool getDefault(const bools ID) {
             return true;
         case showToolbarsInEachWindow:
             return true;
+        case showTipAtStartup:
+            return true;
     }
     return false;
 }
@@ -425,6 +431,8 @@ int getDefault(const ints ID) {
             return 1;
         case configDialogWidth:
             return 1;
+        case tipNumber:
+            return 0;
     }
     return 0;
 }

src/backend/config/cbtconfig.h
@@ -76,7 +76,8 @@ enum bools {
     showTextWindowToolButtons,
     showTextWindowModuleSelectorButtons,
     showFormatToolbarButtons,
-    showToolbarsInEachWindow
+    showToolbarsInEachWindow,
+    showTipAtStartup
 };
 enum ints {
     footnotes,
@@ -113,7 +114,8 @@ enum ints {
     configDialogPosX,
     configDialogPosY,
     configDialogHeight,
-    configDialogWidth
+    configDialogWidth,
+    tipNumber
 };
 enum intLists {
     leftPaneSplitterSizes,

src/bibletime.h
@@ -226,6 +226,11 @@ class BibleTime : public QMainWindow {
         void openOnlineHelp_Howto();
 
         /**
+        * Open the Tip Dialog
+        */
+        void slotOpenTipDialog();
+
+        /**
           Processes the command-line options given to BibleTime.
           \param[in] ignoreSession Specifies whether --ignore-session was used.
           \param[in] bibleKey If --open-default-bible was used, the bible key
@@ -503,6 +508,7 @@ class BibleTime : public QMainWindow {
         QAction *m_openHandbookAction;
         QAction *m_bibleStudyHowtoAction;
         QAction *m_aboutBibleTimeAction;
+        QAction *m_tipOfTheDayAction;
 
         BtActionCollection* m_actionCollection;
 

src/bibletime_slots.cpp
@@ -34,6 +34,7 @@
 #include "frontend/profile/cprofilewindow.h"
 #include "frontend/searchdialog/csearchdialog.h"
 #include "frontend/settingsdialogs/cconfigurationdialog.h"
+#include "frontend/tips/bttipdialog.h"
 #include "util/directory.h"
 
 
@@ -410,6 +411,11 @@ void BibleTime::openOnlineHelp_Howto() {
 
 void BibleTime::slotOpenAboutDialog() {
     BtAboutDialog* dlg = new BtAboutDialog(this);
+    dlg->show();
+}
+
+void BibleTime::slotOpenTipDialog() {
+    BtTipDialog* dlg = new BtTipDialog(this);
     dlg->show();
 }
 

commit d07db86b7c91838969a0a950497cd4a9b040ccce

Fixed color issues of BtTipDialog, a tooltip string and a ChangeLog typo introduced by e0d156632f6fde4ef291573de26242dca415f87f.

src/bibletime_init.cpp
@@ -261,7 +261,7 @@ void BibleTime::insertKeyboardActions( BtActionCollection* const a ) {
     action->setText(tr("&Tip of the day..."));
     action->setIcon(DU::getIcon(CResMgr::mainMenu::help::tipOfTheDay::icon));
     action->setShortcut(QKeySequence(CResMgr::mainMenu::help::tipOfTheDay::accel));
-    action->setToolTip(tr("Tips about the BibleTime program"));
+    action->setToolTip(tr("Show tips about BibleTime"));
     a->addAction("tipOfTheDay", action);
 
     action = new QAction(a);

src/frontend/tips/bttipdialog.cpp
@@ -102,12 +102,15 @@ static QString vertical_align(const QString& text)
 
 static QString make_style(QWidget* widget)
 {
-    return "<style type=\"text/css\">"\
-     "body{}"\
-    "h3{font-weight:bold;text-align:center}"\
-    "a{text-decoration:underline}"\
-    "a:link{color:" + widget->palette().color(QPalette::Link).name() + "}"\
-    "a:visited{color:" + widget->palette().color(QPalette::LinkVisited).name() + "}"\
+    const QPalette &p = widget->palette();
+    return "<style type=\"text/css\">"
+        "body{"
+            "background-color:" + p.color(QPalette::Base).name() + ";"
+            "color:" + p.color(QPalette::Text).name() + "}"
+        "h3{font-weight:bold;text-align:center}"
+        "a{text-decoration:underline}"
+        "a:link{color:" + p.color(QPalette::Link).name() + "}"
+        "a:visited{color:" + p.color(QPalette::LinkVisited).name() + "}"
     "</style>";
 }
 
@@ -158,7 +161,7 @@ void BtTipDialog::create_tips() {
         " into the Bookmarks window. An arrow will indicate the position that the bookmark will"
         " go when you release the cursor. Other works will have a reference in the upper left"
        " corner that can be used to create a bookmark.");
- 
+
     m_tips << tr("To change a bookmark title or description, right click on the bookmark"
         " and select \"Edit Bookmark...\". After finishing the edit the description can be"
         " seen by hovering over the bookmark.");