5222cb52634f1d6be5934237beb441f437c84d29
patrick <patrick@zakweb.de>
2011-01-13 00:01:53 +0100

    Add session tests.
    Signed-off-by: patrick <patrick@zakweb.de>

--- src/tests/btconfig_test.cpp
+++ src/tests/btconfig_test.cpp
@@ -19,38 +19,82 @@ private slots:
         m_btConfig = new BtConfig(m_configFileName);
     }
 
-    void addingConfigOptions()
+    void cleanupTestCase()
+    {
+        delete m_btConfig;
+        QFile(m_configFileName).remove();
+    }
+
+    void gettingNonSetOptionsTest()
+    {
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "KJV");
+        QVERIFY(m_btConfig->getValue("presentation/verseNumbers").toBool() == false);
+        QVERIFY(m_btConfig->getValue("gui/windows/searchType").value<Search::BtSearchOptionsArea::SearchType>() == Search::BtSearchOptionsArea::AndType);
+    }
+
+    void gettingSettingsOptionsTest()
     {
         m_btConfig->setValue("settings/defaults/standardBible", "Lut1940");
         m_btConfig->setValue("gui/showTextWindowHeaders", false);
         m_btConfig->setValue<btconfigtypes::alignmentMode>("gui/alignmentMode", btconfigtypes::autoCascade);
-        QVERIFY(true);
-    }
-
-    void retrievingSetOptions()
-    {
         QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "Lut1940");
         QVERIFY(m_btConfig->getValue("gui/showTextWindowHeaders").toBool() == false);
         QVERIFY(m_btConfig->getValue("gui/alignmentMode").value<btconfigtypes::alignmentMode>() == btconfigtypes::autoCascade);
     }
 
-    void retrievingNonSetOptions()
+    void deletingOptionsTest()
     {
-        QVERIFY(m_btConfig->getValue("settings/defaults/standardCommentary").toString() == "MHC");
-        QVERIFY(m_btConfig->getValue("presentation/verseNumbers").toBool() == false);
-        QVERIFY(m_btConfig->getValue("gui/windows/searchType").value<Search::BtSearchOptionsArea::SearchType>() == Search::BtSearchOptionsArea::AndType);
-    }
-
-    void deletingOptions()
-    {
+        m_btConfig->setValue("settings/defaults/standardBible", "Lut1940");
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "Lut1940");
         m_btConfig->deleteValue("settings/defaults/standardBible");
         QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "KJV");
     }
 
-    void cleanupTestCase()
+    void sessionNamesTest()
     {
-        delete m_btConfig;
-        QFile(m_configFileName).remove();
+
+        QVERIFY(m_btConfig->getCurrentSessionName() == "default session");
+        QVERIFY(m_btConfig->getAllSessionNames().size() == 1);
+        m_btConfig->switchToSession("One");
+        m_btConfig->switchToSession("Two");
+        m_btConfig->switchToSession("Three");
+        QVERIFY(m_btConfig->getAllSessionNames().size() == 4);
+
+        QVERIFY(m_btConfig->getCurrentSessionName() == "Three");
+    }
+
+    void switchingSessionTest()
+    {
+        m_btConfig->switchToSession("First session");
+        m_btConfig->setValue("settings/defaults/standardBible", "neUe");
+        m_btConfig->setValue("gui/showTextWindowHeaders", true);
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "neUe");
+        QVERIFY(m_btConfig->getValue("gui/showTextWindowHeaders").toBool() == true);
+
+        m_btConfig->switchToSession("Second session");
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "KJV");
+        QVERIFY(m_btConfig->getValue("gui/showTextWindowHeaders").toBool() == false);
+
+        m_btConfig->switchToSession("First session");
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "neUe");
+        QVERIFY(m_btConfig->getValue("gui/showTextWindowHeaders").toBool() == true);
+    }
+
+    void deletingSessionTest()
+    {
+        m_btConfig->switchToSession("Third session");
+        m_btConfig->setValue("settings/defaults/standardBible", "neUe");
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "neUe");
+
+        QVERIFY(m_btConfig->deleteSession("First Session") == false);
+
+        m_btConfig->switchToSession("Fourth session");
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "KJV");
+
+        QVERIFY(m_btConfig->deleteSession("First Session") == true);
+
+        m_btConfig->switchToSession("Third session");
+        QVERIFY(m_btConfig->getValue("settings/defaults/standardBible").toString() == "KJV");
     }
 };
 
