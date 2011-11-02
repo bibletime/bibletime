d74e7462bc58ab4e98800d0502aaaee2aaf76575
Jaak Ristioja <jaak@ristioja.ee>
2011-11-02 02:29:53 +0200

    Added missing licenses to source files.

--- src/backend/config/btconfigcore.cpp
+++ src/backend/config/btconfigcore.cpp
@@ -1,3 +1,12 @@
+/*********
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2011 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
 #include "btconfigcore.h"
 
 #if __cplusplus >= 201103L
