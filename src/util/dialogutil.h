c84a78e4e859c78679b36c9aceb00ae161d7499a
Jaak Ristioja <jaak@ristioja.ee>
2012-04-07 13:20:37 +0300

    Small fixes to comments in util/dialogutil.h.

--- src/util/dialogutil.h
+++ src/util/dialogutil.h
@@ -16,16 +16,16 @@ class QDialogButtonBox;
 
 namespace util {
 
-/*
-* Translate standard buttons in a QDialogButtonBox. Check that all used buttons are translated
-* in the implementation.
+/**
+  Translates standard buttons in a QDialogButtonBox.
 */
 void prepareDialogBox(QDialogButtonBox *box);
 
 /*
-* Use util::showWarning() etc. instead of QMessageBox static functions.
-* QMessageBox button texts are not translated trustworthily.
+  Use util::showWarning() etc. instead of QMessageBox static functions.
+  QMessageBox button texts are not translated trustworthily.
 */
+
 QMessageBox::StandardButton showWarning(QWidget * parent, const QString & title, const QString & text, QMessageBox::StandardButtons buttons = QMessageBox::Ok, QMessageBox::StandardButton defaultButton = QMessageBox::NoButton);
 
 QMessageBox::StandardButton showCritical(QWidget * parent, const QString & title, const QString & text, QMessageBox::StandardButtons buttons = QMessageBox::Ok, QMessageBox::StandardButton defaultButton = QMessageBox::NoButton);
