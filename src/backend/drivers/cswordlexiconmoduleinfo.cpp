7a96653d35488e97732b64b073002c143f859a3d
Jaak Ristioja <jaak@ristioja.ee>
2013-05-20 01:47:44 +0300

    Fixed an old Qt3->Qt4 porting error from 2eca51d3 (SVN 77), minor optimizations to code.

--- src/backend/drivers/cswordlexiconmoduleinfo.cpp
+++ src/backend/drivers/cswordlexiconmoduleinfo.cpp
@@ -100,13 +100,8 @@ const QStringList &CSwordLexiconModuleInfo::entries() const {
     module()->setPosition(sword::TOP); // back to the first entry
     module()->setSkipConsecutiveLinks(false);
 
-    if (m_entries.count()) {
-        m_entries.first().simplified();
-
-        if (m_entries.first().trimmed().isEmpty()) {
-            m_entries.erase( m_entries.begin() );
-        }
-    }
+    if (!m_entries.empty() && m_entries.front().simplified().isEmpty())
+        m_entries.pop_front();
 
     qDebug() << "Writing cache file for lexicon module" << name();
 
