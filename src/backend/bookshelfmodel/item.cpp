16a92c1d1268a3f045f04f6f65fb381df84b82ca
Jaak Ristioja <jaak@ristioja.ee>
2013-06-09 19:17:37 +0300

    BookshelfModel::Item: code style fixes, inlined constructor, made some protected stuff private.

--- src/backend/bookshelfmodel/item.cpp
+++ src/backend/bookshelfmodel/item.cpp
@@ -17,56 +17,56 @@
 
 namespace BookshelfModel {
 
-Item::Item(Type type)
-        : m_type(type), m_parent(0), m_checkState(Qt::Unchecked) {
-    // Intentionally empty
-}
-
 Item::~Item() {
     qDeleteAll(m_children);
 }
 
-int Item::indexFor(Item *newItem) {
-    Q_ASSERT(newItem != 0);
+int Item::indexFor(Item * newItem) {
+    Q_ASSERT(newItem);
 
-    if (m_children.empty()) return 0;
+    if (m_children.empty())
+        return 0;
 
-    int i(0);
+    int i = 0;
     for (;;) {
-        Item *nextItem(m_children.at(i));
+        Item * const nextItem(m_children.at(i));
         Q_ASSERT(nextItem->type() == newItem->type());
-        if (*newItem < *nextItem) {
+        if (*newItem < *nextItem)
             return i;
-        }
+
         i++;
-        if (i >= m_children.size()) {
+        if (i >= m_children.size())
             return i;
-        }
     }
 }
 
 QVariant Item::data(int role) const {
     switch (role) {
+
         case Qt::CheckStateRole:
             return m_checkState;
-        case BtBookshelfModel::ModuleHiddenRole:
-            if (m_children.empty()) return true;
 
-            foreach (Item *child, m_children) {
-                if (!child->data(role).toBool()) return false;
-            }
+        case BtBookshelfModel::ModuleHiddenRole:
+            if (m_children.empty())
+                return true;
+
+            Q_FOREACH (Item * child, m_children)
+                if (!child->data(role).toBool())
+                    return false;
             return true;
+
         default:
             return QVariant();
+
     }
 }
 
-bool Item::operator<(const Item &other) const {
-    if (m_type != other.type()) {
+bool Item::operator<(const Item & other) const {
+    if (m_type != other.type())
         return m_type < other.type();
-    }
-    QString first(data(Qt::DisplayRole).toString().toLower());
-    QString second(other.data(Qt::DisplayRole).toString().toLower());
+
+    const QString first(data(Qt::DisplayRole).toString().toLower());
+    const QString second(other.data(Qt::DisplayRole).toString().toLower());
     return first.localeAwareCompare(second) < 0;
 }
 
--- src/backend/bookshelfmodel/item.h
+++ src/backend/bookshelfmodel/item.h
@@ -25,134 +25,150 @@ class CSwordModuleInfo;
 namespace BookshelfModel {
 
 class Item {
-    public:
-        enum Type {
-            ITEM_ROOT         = 0,
-            ITEM_CATEGORY     = 1,
-            ITEM_LANGUAGE     = 2,
-            ITEM_MODULE       = 3,
-            ITEM_INDEXING     = 4
-        };
 
-        Item(Type type);
-        virtual ~Item();
+public: /* Types: */
 
-        /**
-          \brief Returns the type of this item.
-        */
-        inline Type type() const {
-            return m_type;
-        }
+    enum Type {
+        ITEM_ROOT         = 0,
+        ITEM_CATEGORY     = 1,
+        ITEM_LANGUAGE     = 2,
+        ITEM_MODULE       = 3,
+        ITEM_INDEXING     = 4
+    };
 
-        /**
-          \brief Returns a pointer to the parent item of this item.
-          \retval 0 if this item has no parent.
-        */
-        inline Item *parent() const {
-            return m_parent;
-        }
+public: /* Methods: */
 
-        /**
-          \brief Returns the list of child items of this node.
-        */
-        inline QList<Item*> &children() {
-            return m_children;
-        }
+    inline Item(Type type)
+        : m_type(type)
+        , m_parent(0)
+        , m_checkState(Qt::Unchecked) {}
 
-        /**
-          \brief Returns the index of this item under its parent.
-          \retval -1 if this item has no parent.
-        */
-        inline int childIndex() const {
-            if (m_parent == 0) return -1;
-            return m_parent->m_children.indexOf(const_cast<Item*>(this));
-        }
+    virtual ~Item();
 
-        /**
-          \brief Returns the position for where the given child item would be
-                 inserted.
-          \param[in] newItem Pointer to the item that would be inserted.
-        */
-        int indexFor(Item *newItem);
+    /**
+      \brief Returns the type of this item.
+    */
+    inline Type type() const {
+        return m_type;
+    }
 
-        /**
-          \brief Inserts the given item as a child at the given index.
-          \pre The given index is a valid position for the item.
-          \param[in] index The child index to insert the item at.
-          \param[in] newItem The item to insert.
-        */
-        inline void insertChild(int index, Item *newItem) {
-            Q_ASSERT(newItem != 0);
-            Q_ASSERT(index >= 0 && index <= m_children.size());
-            m_children.insert(index, newItem);
-            newItem->setParent(this);
-        }
+    /**
+      \brief Returns a pointer to the parent item of this item.
+      \retval 0 if this item has no parent.
+    */
+    inline Item * parent() const {
+        return m_parent;
+    }
 
-        template <class T>
-        T *getGroupItem(CSwordModuleInfo *module, int &outIndex) {
-            for (int i(0); i < m_children.size(); i++) {
-                Q_ASSERT(m_children.at(i)->type() == T::GROUP_TYPE);
-                T *item(static_cast<T*>(m_children.at(i)));
-                if (item->fitFor(module)) {
-                    outIndex = i;
-                    return item;
-                }
+    /**
+      \brief Returns the list of child items of this node.
+    */
+    inline QList<Item *> & children() {
+        return m_children;
+    }
+
+    /**
+      \brief Returns the index of this item under its parent.
+      \retval -1 if this item has no parent.
+    */
+    inline int childIndex() const {
+        return m_parent == 0
+               ? -1
+               : m_parent->m_children.indexOf(const_cast<Item *>(this));
+    }
+
+    /**
+      \brief Returns the position for where the given child item would be
+             inserted.
+      \param[in] newItem Pointer to the item that would be inserted.
+    */
+    int indexFor(Item * newItem);
+
+    /**
+      \brief Inserts the given item as a child at the given index.
+      \pre The given index is a valid position for the item.
+      \param[in] index The child index to insert the item at.
+      \param[in] newItem The item to insert.
+    */
+    inline void insertChild(int index, Item * newItem) {
+        Q_ASSERT(newItem != 0);
+        Q_ASSERT(index >= 0 && index <= m_children.size());
+        m_children.insert(index, newItem);
+        newItem->setParent(this);
+    }
+
+    template <class T>
+    inline T * getGroupItem(CSwordModuleInfo * module, int & outIndex) {
+        for (int i = 0; i < m_children.size(); i++) {
+            Q_ASSERT(m_children.at(i)->type() == T::GROUP_TYPE);
+            T * item = static_cast<T *>(m_children.at(i));
+            if (item->fitFor(module)) {
+                outIndex = i;
+                return item;
             }
-            return 0;
         }
+        return 0;
+    }
 
-        /**
-          \brief Returns data for this item.
-        */
-        virtual QVariant data(int role = Qt::DisplayRole) const;
+    /**
+      \brief Returns data for this item.
+    */
+    virtual QVariant data(int role = Qt::DisplayRole) const;
 
-        /**
-          \brief Returns the check state of this item.
-        */
-        inline Qt::CheckState checkState() const {
-            return m_checkState;
-        }
+    /**
+      \brief Returns the check state of this item.
+    */
+    inline Qt::CheckState checkState() const {
+        return m_checkState;
+    }
 
-        /**
-          \brief Sets the check state of this item.
-          \param[in] state new check state.
-        */
-        inline void setCheckState(const Qt::CheckState state) {
-            m_checkState = state;
-        }
+    /**
+      \brief Sets the check state of this item.
+      \param[in] state new check state.
+    */
+    inline void setCheckState(const Qt::CheckState state) {
+        m_checkState = state;
+    }
 
-        /**
-          \brief Returns whether this item is fit to contain the given module.
-          \param[in] module The module to check with.
-          \retval true If this item is a group and can contain the given module.
-          \retval false This item is not a group or is a wrong group.
-        */
-        inline virtual bool fitFor(CSwordModuleInfo *module) const {
-            Q_UNUSED(module);
-            return false;
-        }
+    /**
+      \brief Returns whether this item is fit to contain the given module.
+      \param[in] module The module to check with.
+      \retval true If this item is a group and can contain the given module.
+      \retval false This item is not a group or is a wrong group.
+    */
+    inline virtual bool fitFor(CSwordModuleInfo * module) const {
+        Q_UNUSED(module);
+        return false;
+    }
 
-        /**
-          \brief Comparsion operator used sorting child items.
-        */
-        virtual bool operator<(const Item &other) const;
+    /**
+      \brief Comparsion operator used sorting child items.
+    */
+    virtual bool operator<(const Item & other) const;
 
-    protected:
-        inline void setParent(Item *parent) {
-            Q_ASSERT(parent != 0);
-            m_parent = parent;
-        }
+private: /* Methods: */
 
-    protected:
-        Type           m_type;
-        Item          *m_parent;
-        QList<Item*>   m_children;
-        Qt::CheckState m_checkState;
+    inline void setParent(Item * parent) {
+        Q_ASSERT(parent != 0);
+        m_parent = parent;
+    }
+
+private: /* Fields: */
+
+    Type m_type;
+    Item * m_parent;
+    QList<Item *> m_children;
+    Qt::CheckState m_checkState;
+
 };
 
 class RootItem: public Item {
-    public:
-        inline RootItem() : Item(Item::ITEM_ROOT) {}
+
+public: /* Methods: */
+
+    inline RootItem()
+        : Item(Item::ITEM_ROOT) {}
+
 };
 
 } // Namespace BookshelfModel
