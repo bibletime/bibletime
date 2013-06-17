8db6e855ee0c06f2e12587d586e64fc3101a1079
Gary Holmlund <gary.holmlund@gmail.com>
2013-06-17 16:33:41 -0700

    Add copyright notices

--- src/mobile/bibletimeapp.h
+++ src/mobile/bibletimeapp.h
@@ -4,8 +4,9 @@
 *
 * This file is part of BibleTime's source code, http://www.bibletime.info/.
 *
-* Copyright 1999-2011 by the BibleTime developers.
-* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+* Copyright 1999-2013 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License
+* version 2.0.
 *
 **********/
 
