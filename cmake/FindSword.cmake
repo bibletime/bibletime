5997cb0a52d306f4a9ff62070adaa8d3b7d8c785
Jaak Ristioja <jaak@ristioja.ee>
2013-11-04 00:20:53 +0200

    Added license to FindSword.cmake.

--- cmake/FindSword.cmake
+++ cmake/FindSword.cmake
@@ -1,4 +1,13 @@
-# This module looks for installed Sword
+#
+# In the name of the Father, and of the Son, and of the Holy Spirit.
+#
+# This file is part of BibleTime's source code, http://www.bibletime.info/.
+#
+# Copyright 1999-2013 by the BibleTime developers.
+# The BibleTime source code is licensed under the GNU General Public License version 2.0.
+#
+
+# This module looks for the Sword library by the Crosswire Bible Society
 #
 # It will define the following values
 # Sword_FOUND
