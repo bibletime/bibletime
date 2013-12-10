a9361607c38065823827b34509ea5ba857990c2a
Jaak Ristioja <jaak@ristioja.ee>
2013-12-10 23:20:15 +0200

    Changed source code archive format from .tar.bz2 to .tar.xz.

--- archive.sh
+++ archive.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 FULLNAME="bibletime-$1"
 TARNAME="${FULLNAME}.tar"
-git archive --format=tar "--prefix=${FULLNAME}/" -o "$TARNAME" HEAD && bzip2 --best "$TARNAME" || exit
-TARBZ2NAME="${TARNAME}.bz2"
-BYTES=`stat '--format=%s' "$TARBZ2NAME"`
+git archive --format=tar "--prefix=${FULLNAME}/" -o "$TARNAME" HEAD && xz -9 "$TARNAME" || exit
+TARXZNAME="${TARNAME}.xz"
+BYTES=`stat '--format=%s' "$TARXZNAME"`
 echo 'Details:'
-echo "  Filename:  ${TARBZ2NAME}"
+echo "  Filename:  ${TARXZNAME}"
 echo "  File size: $BYTES bytes`echo $BYTES|awk '{s[2**30]="G";s[2**20]="M";hum[1024]="k";for(x=2**30;x>=1024;x/=1024){if($1>=x){printf " (%.2f %sB)\n",$1/x,s[x];break}}}'`"
-echo "  MD5SUM:    `md5sum \"${TARBZ2NAME}\" |cut -b -32`"
-echo "  SHA1SUM:   `sha1sum \"${TARBZ2NAME}\" |cut -b -40`"
-echo "  SHA256SUM: `sha256sum \"${TARBZ2NAME}\" |cut -b -64`"
+echo "  MD5SUM:    `md5sum \"${TARXZNAME}\" |cut -b -32`"
+echo "  SHA1SUM:   `sha1sum \"${TARXZNAME}\" |cut -b -40`"
+echo "  SHA256SUM: `sha256sum \"${TARXZNAME}\" |cut -b -64`"
