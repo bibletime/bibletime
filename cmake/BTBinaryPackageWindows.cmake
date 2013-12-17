9cc46d09a0d6171e8c2888b7edf8141ce19ef362
Gary Holmlund <gary.holmlund@gmail.com>
2013-12-16 20:34:39 -0800

    Cmake script changes for Windows

--- cmake/BTBinaryPackageWindows.cmake
+++ cmake/BTBinaryPackageWindows.cmake
@@ -22,24 +22,61 @@ IF(WIN32 AND NOT UNIX)
 IF (Qt5Core_FOUND)
     SET(QT_BINARY_DIR "${Qt5Core_DIR}/../../../bin")
     INSTALL(FILES
-        "${QT_BINARY_DIR}/Qt5WebKitd.dll"
-        "${QT_BINARY_DIR}/Qt5Guid.dll"
-        "${QT_BINARY_DIR}/Qt5Xmld.dll"
-        "${QT_BINARY_DIR}/Qt5Networkd.dll"
+        "${QT_BINARY_DIR}/icudt51d.dll"
+        "${QT_BINARY_DIR}/icuin51d.dll"
+        "${QT_BINARY_DIR}/icuuc51d.dll"
+        "${QT_BINARY_DIR}/libEGLd.dll"
+        "${QT_BINARY_DIR}/libGLESv2d.dll"
         "${QT_BINARY_DIR}/Qt5Cored.dll"
+        "${QT_BINARY_DIR}/Qt5Guid.dll"
+        "${QT_BINARY_DIR}/Qt5Multimediad.dll"
+        "${QT_BINARY_DIR}/Qt5MultimediaWidgetsd.dll"
+        "${QT_BINARY_DIR}/Qt5Networkd.dll"
+        "${QT_BINARY_DIR}/Qt5OpenGLd.dll"
+        "${QT_BINARY_DIR}/Qt5Qmld.dll"
+        "${QT_BINARY_DIR}/Qt5Quickd.dll"
+        "${QT_BINARY_DIR}/Qt5Positioningd.dll"
+        "${QT_BINARY_DIR}/Qt5PrintSupportd.dll"
+        "${QT_BINARY_DIR}/Qt5Sensorsd.dll"
+        "${QT_BINARY_DIR}/Qt5Sqld.dll"
         "${QT_BINARY_DIR}/Qt5Svgd.dll"
+        "${QT_BINARY_DIR}/Qt5WebKitd.dll"
+        "${QT_BINARY_DIR}/Qt5WebKitWidgetsd.dll"
+        "${QT_BINARY_DIR}/Qt5Widgetsd.dll"
+        "${QT_BINARY_DIR}/Qt5Xmld.dll"
         "${QT_BINARY_DIR}/Qt5XmlPatternsd.dll"
         DESTINATION "${BT_DESTINATION}"
         CONFIGURATIONS "Debug"
     )
     INSTALL(FILES
-        "${QT_BINARY_DIR}/Qt5WebKit.dll"
-        "${QT_BINARY_DIR}/Qt5Gui.dll"
-        "${QT_BINARY_DIR}/Qt5Xml.dll"
-        "${QT_BINARY_DIR}/Qt5Network.dll"
+        "${QT_BINARY_DIR}/icudt51.dll"
+        "${QT_BINARY_DIR}/icuin51.dll"
+        "${QT_BINARY_DIR}/icuuc51.dll"
+        "${QT_BINARY_DIR}/libEGL.dll"
+        "${QT_BINARY_DIR}/libGLESv2.dll"
         "${QT_BINARY_DIR}/Qt5Core.dll"
+        "${QT_BINARY_DIR}/Qt5Gui.dll"
+        "${QT_BINARY_DIR}/Qt5Multimedia.dll"
+        "${QT_BINARY_DIR}/Qt5MultimediaWidgets.dll"
+        "${QT_BINARY_DIR}/Qt5Network.dll"
+        "${QT_BINARY_DIR}/Qt5OpenGL.dll"
+        "${QT_BINARY_DIR}/Qt5Qml.dll"
+        "${QT_BINARY_DIR}/Qt5Quick.dll"
+        "${QT_BINARY_DIR}/Qt5Positioning.dll"
+        "${QT_BINARY_DIR}/Qt5PrintSupport.dll"
+        "${QT_BINARY_DIR}/Qt5Sensors.dll"
+        "${QT_BINARY_DIR}/Qt5Sql.dll"
         "${QT_BINARY_DIR}/Qt5Svg.dll"
+        "${QT_BINARY_DIR}/Qt5WebKit.dll"
+        "${QT_BINARY_DIR}/Qt5WebKitWidgets.dll"
+        "${QT_BINARY_DIR}/Qt5Widgets.dll"
+        "${QT_BINARY_DIR}/Qt5Xml.dll"
         "${QT_BINARY_DIR}/Qt5XmlPatterns.dll"
+        DESTINATION "${BT_DESTINATION}"
+        CONFIGURATIONS "Release"
+    )
+	INSTALL(FILES
+	    "cmake/platforms/windows/qt.conf"
         DESTINATION "${BT_DESTINATION}"
         CONFIGURATIONS "Release"
     )
@@ -47,6 +84,13 @@ IF (Qt5Core_FOUND)
     INSTALL(FILES
         "${QT_PLUGINS_DIR}/iconengines/qsvgicon.dll"
         DESTINATION "${BT_DESTINATION}/plugins/iconengines"
+        CONFIGURATIONS "Release"
+    )
+    INSTALL(FILES
+        "${QT_PLUGINS_DIR}/platforms/qminimal.dll"
+        "${QT_PLUGINS_DIR}/platforms/qoffscreen.dll"
+        "${QT_PLUGINS_DIR}/platforms/qwindows.dll"
+        DESTINATION "${BT_DESTINATION}/plugins/platforms"
         CONFIGURATIONS "Release"
     )
     INSTALL(FILES
@@ -124,10 +168,6 @@ ENDIF (Qt5Core_FOUND)
     MESSAGE(STATUS  "INSTALL CURL_LIBRARY ${CURL_LIBRARY}" )
     STRING(REPLACE "_imp.lib" ".dll" CURL_DLL "${CURL_LIBRARY}")
     INSTALL(FILES ${CURL_DLL} DESTINATION ${BT_DESTINATION})
-
-    MESSAGE(STATUS  "INSTALL ZLIB_LIBRARY ${ZLIB_LIBRARY}" )
-    STRING(REPLACE ".lib" ".dll" ZLIB_DLL "${ZLIB_LIBRARY}")
-    INSTALL(FILES ${ZLIB_DLL} DESTINATION ${BT_DESTINATION})
 
     # Some options for the CPack system.  These should be pretty self-evident
     SET(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}\\\\pics\\\\icons\\\\bibletime.png")
