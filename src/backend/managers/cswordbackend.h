bb61c31dc0e005b32e69673670cb93a0c64d9b74
Jaak Ristioja <jaak@ristioja.ee>
2013-12-22 20:57:15 +0200

    backend, CSwordBackend: Optimizations, code and comments formatting.

--- src/backend/managers/cswordbackend.h
+++ src/backend/managers/cswordbackend.h
@@ -12,11 +12,9 @@
 #ifndef CSWORDBACKEND_H
 #define CSWORDBACKEND_H
 
-#include <QMap>
 #include <QObject>
 #include <QString>
 #include <QStringList>
-#include <QVariant>
 #include "backend/drivers/cswordmoduleinfo.h"
 #include "backend/bookshelfmodel/btbookshelfmodel.h"
 #include "backend/filters/gbftohtml.h"
@@ -47,215 +45,241 @@
         BibleTimeApp::~BibleTimeApp(). Only when \ref BackendNotSingleton
         "managing modules" separate backends are created.
 */
-class CSwordBackend : public QObject, public sword::SWMgr {
-        Q_OBJECT
-    public:
+class CSwordBackend: public QObject, public sword::SWMgr {
 
-        /** The reason for the sigSwordSetupChanged signal, i.e. why the module list has changed. */
-        enum SetupChangedReason {
-            AddedModules = 1,
-            RemovedModules = 2,
-            HidedModules = 4,
-            PathChanged = 8,
-            OtherChange = 16
-        };
+    Q_OBJECT
 
-        /** The error codes which may be returned by the @ref Load() call.
-        */
-        enum LoadError { // the values exist to cast from the char return of SWMgr::Load
-            NoSwordConfig = -1,
-            NoError = 0,
-            NoModules = 1
-        };
-        /**
-        * The constructor of the Sword backend. Used by BtInstallBackend only.
-        * Notice that using augmentHome=false can mess up the system because it is true elsewhere.
-        * @param path The path which is used to load modules
-        * @param augmentHome True if the $HOME/.sword/ modules should be augmented with the other modules
-        */
-        CSwordBackend( const QString& path, const bool augmentHome = true );
+public: /* Types: */
 
-        /**
-        * The destrctor of this backend. This function shuts the modules down using @ref shutdownModules.
-        */
-        ~CSwordBackend();
+    /**
+      \brief The reason for the sigSwordSetupChanged signal, i.e. why the module
+             list has changed.
+    */
+    enum SetupChangedReason {
+        AddedModules = 1,
+        RemovedModules = 2,
+        HidedModules = 4,
+        PathChanged = 8,
+        OtherChange = 16
+    };
 
-        /** Creates and returns the instance. */
-        static inline CSwordBackend *createInstance() {
-            Q_ASSERT(m_instance == 0);
-            m_instance = new CSwordBackend();
-            return m_instance;
-        }
+    /**
+      \brief The error codes which may be returned by the \ref Load() call.
+      \note These values exist to cast from the char return of SWMgr::Load().
+    */
+    enum LoadError {
+        NoSwordConfig = -1,
+        NoError = 0,
+        NoModules = 1
+    };
 
-        /** Returns the singleton instance, creating it if one does not exist. */
-        static inline CSwordBackend *instance() { return m_instance; }
+public: /* Methods: */
 
-        /** Destroys the singleton instance, if one exists. */
-        static void destroyInstance() { delete m_instance; }
+    /**
+      \note Used by BtInstallBackend only.
+      \note Using augmentHome=false can mess up the system because it is true
+            elsewhere.
+      \param[in] path The path which is used to load modules.
+      \param[in] augmentHome Whether $HOME/.sword/ modules should be augmented
+                             with the other modules.
+    */
+    CSwordBackend(const QString & path, const bool augmentHome = true);
 
-        /**
-        * This function returns the list of available modules managed by this
-        * backend. You have to call initModules() first; This method is
-        * equivalent to model()->modules().
-        *
-        * @return The list of modules managed by this backend
-        */
-        inline const QList<CSwordModuleInfo*>& moduleList() const;
+    ~CSwordBackend();
 
-        inline BtBookshelfModel *model();
+    /** \returns the CSwordBackend singleton instance (created if needed). */
+    static inline CSwordBackend * createInstance() {
+        Q_ASSERT(!m_instance);
+        m_instance = new CSwordBackend();
+        return m_instance;
+    }
 
-        /**
-        * Initializes the Sword modules.
-        *
-        * @return True if the initializiation was succesful, otherwise return false.
-        */
-        CSwordBackend::LoadError initModules(SetupChangedReason reason);
-        /**
-        * This function deinitializes the modules and deletes them.
-        *
-        * @return True if it was succesful, otherwise return false
-        */
-        bool shutdownModules();
-        /**
-        * Sets the given options enabled or disabled depending on the second parameter.
-        *
-        * @param type This is the type this function should set enabled or disabled
-        * @param enable If this is true the option will be enabled, otherwise it will be disabled.
-        */
-        void setOption( const CSwordModuleInfo::FilterTypes type, const int state );
+    /** \returns the singleton instance, creating it if one does not exist. */
+    static inline CSwordBackend * instance() { return m_instance; }
 
-        void setFilterOptions(const FilterOptions &options);
-        /**
-        * Sets the language for the international booknames of Sword.
-        * @param langName The abbreviation string which should be used for the Sword backend
-        */
-        const QString booknameLanguage( const QString& langName = QString::null );
-        /**
-        * This function searches for a module with the specified description
-        * @param description The description of the desired module
-        * @return pointer to the desired module; null if no module has the specified description
-        */
-        CSwordModuleInfo* findModuleByDescription(const QString &description) const;
+    /** \brief Destroys the singleton instance, if one exists. */
+    static inline void destroyInstance() {
+        delete m_instance;
+        m_instance = NULL;
+    }
 
-        /**
-        * This function searches for a module with the specified name
-        * @param name The name of the desired module
-        * @return Pointer to the desired module; null if no module has the specified name
-        */
-        CSwordModuleInfo* findModuleByName(const QString &name) const;
-        /**
-        * This function searches for a module with the specified sword module as module() object!
-        * @param swmodule to a Sword module
-        * @return pointer to the desired module; null if no module has the specified name
-        */
-        CSwordModuleInfo* findSwordModuleByPointer(const sword::SWModule * const swmodule) const;
+    /**
+      \warning You have to call initModules() first.
+      \note This method is equivalent to model()->modules().
+      \returns The list of modules managed by this backend.
+    */
+    inline const QList<CSwordModuleInfo *> & moduleList() const;
 
-        /**
-        * @return Our global config object which contains the configs of all modules merged together.
-        */
-        inline sword::SWConfig* getConfig() const;
+    inline BtBookshelfModel * model();
 
-        /**
-        * Returns the text used for the option given as parameter.
-        * @param The paramter enum
-        * @return The name of the option given by the parameter
-        */
-        static const QString optionName( const CSwordModuleInfo::FilterTypes option );
-        /**
-        * Returns the text used for the option given as parameter.
-        */
-        static const QString configOptionName( const CSwordModuleInfo::FilterTypes option );
-        /**
-        * Returns the translated name of the option given as parameter.
-        * @param The translated option name
-        */
-        static const QString translatedOptionName(const CSwordModuleInfo::FilterTypes option );
+    /**
+      \brief Initializes the Sword modules.
+      \returns whether the initializiation was successful.
+    */
+    CSwordBackend::LoadError initModules(const SetupChangedReason reason);
 
-        /**
-        * Reload all Sword modules.
-        */
-        void reloadModules(SetupChangedReason reason);
+    /**
+      \brief Deinitializes and frees the modules.
+      \returns whether the method succeeded.
+    */
+    void shutdownModules();
 
-        /**
-        * Takes off the given modules from the list and returns them.
-        * User must take care of the deletion of the returned CSwordModuleInfo pointers.
-        */
-        QList<CSwordModuleInfo*> takeModulesFromList(const QStringList &names);
+    /**
+      \brief Sets the state of the given filter option.
+      \param[in] type The filter type whose state to set.
+      \param[in] state The new filter option state.
+    */
+    void setOption(const CSwordModuleInfo::FilterTypes type, const int state);
 
-        /**
-          \returns a list of pointers to modules, created from a list of module
-                   names.
-        */
-        QList<CSwordModuleInfo*> getPointerList(const QStringList &names) const;
+    void setFilterOptions(const FilterOptions & options);
 
-        /**
-          \returns a list of pointers to const modules, created from a list of
-                   module names.
-        */
-        QList<const CSwordModuleInfo*> getConstPointerList(const QStringList &names) const;
+    /**
+      \brief Sets the language for the international booknames of Sword.
+      \param[in] langName The abbreviation string which should be used for the
+                          Sword backend.
+    */
+    const QString booknameLanguage(const QString & langName = QString::null);
 
-        /** Sword prefix list.
-        * @return A list of all known Sword prefix dirs
-        */
-        QStringList swordDirList() const;
-        /**
-         * delete all orphaned indexes (no module present) if autoDeleteOrphanedIndices is true
-         * delete all indices of modules where hasIndex() returns false (because of wrong index version etc.)
-         */
-        void deleteOrphanedIndices();
+    /**
+      \brief Searches for a module with the given description.
+      \param[in] description The description of the desired module.
+      \returns a pointer to the desired module or NULL if not found.
+    */
+    CSwordModuleInfo * findModuleByDescription(const QString & description) const;
 
-    signals:
-        void sigSwordSetupChanged(CSwordBackend::SetupChangedReason reason);
+    /**
+      \brief Searches for a module with the given name.
+      \param[in] name The name of the desired module.
+      \returns a pointer to the desired module or NULL if not found.
+    */
+    CSwordModuleInfo * findModuleByName(const QString & name) const;
 
-    protected:
-        /**
-          Creates the SWModule objects using SWMgr's methods, it adds the
-          necessary filters for the module format.
-        */
-        CSwordBackend();
+    /**
+      \brief Searches for a module with the given sword module as module().
+      \param[in] swmodule The SWModule of the desired module.
+      \returns a pointer to the desired module or NULL if not found.
+    */
+    CSwordModuleInfo * findSwordModuleByPointer(const sword::SWModule * const swmodule) const;
 
-        /**
-        * Reimplemented from sword::SWMgr.
-        */
-        void AddRenderFilters(sword::SWModule *module, sword::ConfigEntMap &section);
-        /**
-        * Overrides Sword filters which appear to be buggy.
-        */
-        void filterInit();
+    /**
+      \returns The global config object containing the configs of all modules
+               merged together.
+    */
+    inline sword::SWConfig * getConfig() const;
 
-        QStringList getSharedSwordConfigFiles() const;
-        QString getPrivateSwordConfigPath() const;
-        QString getPrivateSwordConfigFile() const;
+    /**
+      \param[in] option The option name to return.
+      \returns The name of the option given by the parameter.
+    */
+    static QString optionName(const CSwordModuleInfo::FilterTypes option);
 
-    private: /* Fields: */
-        // Filters:
-        Filters::GbfToHtml   m_gbfFilter;
-        Filters::OsisToHtml  m_osisFilter;
-        Filters::PlainToHtml m_plainFilter;
-        Filters::TeiToHtml   m_teiFilter;
-        Filters::ThmlToHtml  m_thmlFilter;
+    /**
+      \param[in] option The configuration option name to return.
+      \returns the text used for the configuration option given.
+    */
+    static QString configOptionName(const CSwordModuleInfo::FilterTypes option);
 
-        // Displays:
-        Rendering::CChapterDisplay m_chapterDisplay;
-        Rendering::CEntryDisplay   m_entryDisplay;
-        Rendering::CBookDisplay    m_bookDisplay;
+    /**
+      \param[in] option The translated option name.
+      \returns the translated name of the option given.
+    */
+    static QString translatedOptionName(const CSwordModuleInfo::FilterTypes option);
 
-        BtBookshelfModel m_dataModel;
+    /**
+      \brief Reloads all Sword modules.
+      \param[in] reason The reason for the reload.
+    */
+    void reloadModules(const SetupChangedReason reason);
 
-        static CSwordBackend *m_instance;
+    /**
+      \brief Takes off the given modules from the list and returns them.
+      \param[in] names The names of the modules to take.
+      \note User must take care of the deletion of the returned CSwordModuleInfo
+            pointers.
+    */
+    QList<CSwordModuleInfo *> takeModulesFromList(const QStringList & names);
+
+    /**
+      \param[in] names The names of the modules to return.
+      \returns a list of pointers to modules, created from a list of module
+               names.
+    */
+    QList<CSwordModuleInfo *> getPointerList(const QStringList & names) const;
+
+    /**
+      \param[in] names The names of the modules to return.
+      \returns a list of pointers to const modules, created from a list of
+               module names.
+    */
+    QList<const CSwordModuleInfo *> getConstPointerList(const QStringList & names) const;
+
+    /**
+      \brief Sword prefix list.
+      \returns A list of all known Sword prefix dirs
+    */
+    QStringList swordDirList() const;
+
+    /**
+      Deletes all indices of modules where hasIndex() returns false (because of
+      wrong index version etc.) and deletes all orphaned indexes (no module
+      present) if autoDeleteOrphanedIndices is true.
+    */
+    void deleteOrphanedIndices();
+
+signals:
+
+    void sigSwordSetupChanged(CSwordBackend::SetupChangedReason reason);
+
+protected: /* Methods: */
+
+    /**
+      Creates the SWModule objects using SWMgr's methods, it adds the
+      necessary filters for the module format.
+    */
+    CSwordBackend();
+
+    /** Reimplemented from sword::SWMgr. */
+    void AddRenderFilters(sword::SWModule * module,
+                          sword::ConfigEntMap & section);
+
+    /** Overrides Sword filters which appear to be buggy. */
+    void filterInit();
+
+    QStringList getSharedSwordConfigFiles() const;
+    QString getPrivateSwordConfigPath() const;
+    QString getPrivateSwordConfigFile() const;
+
+private: /* Fields: */
+
+    // Filters:
+    Filters::GbfToHtml   m_gbfFilter;
+    Filters::OsisToHtml  m_osisFilter;
+    Filters::PlainToHtml m_plainFilter;
+    Filters::TeiToHtml   m_teiFilter;
+    Filters::ThmlToHtml  m_thmlFilter;
+
+    // Displays:
+    Rendering::CChapterDisplay m_chapterDisplay;
+    Rendering::CEntryDisplay   m_entryDisplay;
+    Rendering::CBookDisplay    m_bookDisplay;
+
+    BtBookshelfModel m_dataModel;
+
+    static CSwordBackend * m_instance;
+
 };
 
 /**Returns The list of modules managed by this backend*/
-inline const QList<CSwordModuleInfo*> &CSwordBackend::moduleList() const {
+inline const QList<CSwordModuleInfo *> & CSwordBackend::moduleList() const {
     return m_dataModel.moduleList();
 }
 
-inline BtBookshelfModel *CSwordBackend::model() {
+inline BtBookshelfModel * CSwordBackend::model() {
     return &m_dataModel;
 }
 
 /** Returns our local config object to store the cipher keys etc. locally for each user. The values of the config are merged with the global config. */
-inline sword::SWConfig* CSwordBackend::getConfig() const {
+inline sword::SWConfig * CSwordBackend::getConfig() const {
     return config;
 }
 
