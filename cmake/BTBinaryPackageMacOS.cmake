0fd0925fe63606608ed00d5ed7b23d41f4dc8af3
Martin Gruner <martin.gruner@otrs.com>
2014-01-28 21:04:38 +0100

    Fixed build problems on MacOS. Thanks to Greg.

--- cmake/BTBinaryPackageMacOS.cmake
+++ cmake/BTBinaryPackageMacOS.cmake
@@ -1,18 +1,45 @@
 IF(APPLE)
     # Qt Plugins
-    INSTALL(
-        DIRECTORY "${QT_PLUGINS_DIR}/iconengines" "${QT_PLUGINS_DIR}/imageformats"
-        DESTINATION "${BT_DESTINATION}/plugins"
-    )
+    IF(Qt5Core_FOUND)
+        GET_TARGET_PROPERTY(ICON_PLUGIN Qt5::QICOPlugin  LOCATION)
+        GET_TARGET_PROPERTY(GIF_PLUGIN  Qt5::QGifPlugin  LOCATION)
+        GET_TARGET_PROPERTY(JPG_PLUGIN  Qt5::QJpegPlugin LOCATION)
+        GET_TARGET_PROPERTY(SVG_PLUGIN  Qt5::QSvgPlugin  LOCATION)
+        GET_TARGET_PROPERTY(TIFF_PLUGIN Qt5::QTiffPlugin LOCATION)
+        GET_TARGET_PROPERTY(BMP_PLUGIN  Qt5::QWbmpPlugin LOCATION)
+        GET_TARGET_PROPERTY(MNG_PLUGIN  Qt5::QMngPlugin  LOCATION)
+        INSTALL(
+            FILES
+                "${ICON_PLUGIN}"
+                "${GIF_PLUGIN}"
+                "${JPG_PLUGIN}"
+                "${SVG_PLUGIN}"
+                "${TIFF_PLUGIN}"
+                "${BMP_PLUGIN}"
+                "${MNG_PLUGIN}"
+            DESTINATION "${BT_DESTINATION}/plugins"
+        )
+    ELSE(Qt5Core_FOUND)
+        INSTALL(
+            DIRECTORY "${QT_PLUGINS_DIR}/iconengines" "${QT_PLUGINS_DIR}/imageformats"
+            DESTINATION "${BT_DESTINATION}/plugins"
+        )
 
-    INSTALL(
-        DIRECTORY "${QT_LIBRARY_DIR}/QtGui.framework/Resources/qt_menu.nib"
-        DESTINATION "${BT_DESTINATION}/../Frameworks/QtGui.framework/Resources"
-    )
+        INSTALL(
+            DIRECTORY "${QT_LIBRARY_DIR}/QtGui.framework/Resources/qt_menu.nib"
+            DESTINATION "${BT_DESTINATION}/../Frameworks/QtGui.framework/Resources"
+        )
+    ENDIF(Qt5Core_FOUND)
 
     INSTALL(
         FILES "${CMAKE_CURRENT_SOURCE_DIR}/cmake/platforms/macos/qt.conf"
         DESTINATION "${BT_SHARE_PATH}/../Resources"
+    )
+
+    # Install the Sword library
+    INSTALL(
+        FILES "${Sword_LIBRARY_DIRS}/lib${Sword_LIBRARIES}-${Sword_VERSION}.dylib"
+        DESTINATION "${BT_DESTINATION}/"
     )
 
     IF (CMAKE_BUILD_TYPE STREQUAL "Release")
@@ -21,12 +48,13 @@ IF(APPLE)
             FILE(GLOB_RECURSE QTPLUGINS
               \"\${CMAKE_INSTALL_PREFIX}/${BT_DESTINATION}/plugins/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
 
+            SET(BU_CHMOD_BUNDLE_ITEMS ON)   # Add write permissions for libs that need it like libssl
             INCLUDE(BundleUtilities)
 
             FIXUP_BUNDLE(
-                \"\${CMAKE_CURRENT_BINARY_DIR}/\${CMAKE_INSTALL_PREFIX}/${BT_DESTINATION}/BibleTime\"
+                \"\${CMAKE_INSTALL_PREFIX}/${BT_DESTINATION}/BibleTime\"
                 \"\${QTPLUGINS}\"
-                \"\${CMAKE_INSTALL_PREFIX}/${BT_DESTINATION}/plugins/imageformats;\${CMAKE_INSTALL_PREFIX}/${BT_DESTINATION}/plugins/iconengines\"
+                \"\"
             )
         ")
 
