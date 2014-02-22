ce5b089b9ec62abea1135956a1c589998072e040
Gary Holmlund <gary.holmlund@gmail.com>
2014-02-21 17:53:30 -0800

    mobile: Add copyright notice to numerous mobile files.

--- src/mobile/ui/treechoosermodel.h
+++ src/mobile/ui/treechoosermodel.h
@@ -1,3 +1,15 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2014 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License
+* version 2.0.
+*
+**********/
+
 #ifndef TREE_CHOOSER_MODEL_H
 #define TREE_CHOOSER_MODEL_H
 
