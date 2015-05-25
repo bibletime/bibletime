60c5cf64a83cf864f6df1277aefac077b3ede63b
Gary Holmlund <gary.holmlund@gmail.com>
2015-05-24 19:49:41 -0700

    mobile: Update Lithuanian translation.

--- i18n/messages/mobile_ui_lt.ts
+++ i18n/messages/mobile_ui_lt.ts
@@ -1,14 +1,12 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="lt">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
 <context>
-    <name></name>
+    <name/>
     <message>
-        <source></source>
+        <source/>
         <translatorcomment>Bibletime User Interface translation
 Copyright (C) 1999-2014 The Bibletime Team, http://www.bibletime.info
 This file is distributed under the same license as the Bibetime package.</translatorcomment>
-        <translation></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
