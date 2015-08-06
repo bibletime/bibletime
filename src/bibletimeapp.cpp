bibletime/bibletime

commit bb9e1e6c3368264419073fc36f7cfe5603ef2ac3

Add support for retina displays.

src/bibletimeapp.cpp
@@ -25,6 +25,8 @@ BibleTimeApp::BibleTimeApp(int &argc, char **argv)
 {
     setApplicationName("bibletime");
     setApplicationVersion(BT_VERSION);
+    // Support for retina displays
+    this->setAttribute(Qt::AA_UseHighDpiPixmaps);
 }
 
 BibleTimeApp::~BibleTimeApp() {

commit eb2a1c9b9a66892347e31ea3a0dd750d0959af21

Fixed build with Qt versions before 5.2.
Breakage from bb9e1e6c3368264419073fc36f7cfe5603ef2ac3 due to
Qt::AA_UseHighDpiPixmaps not being available for older versions of Qt.

src/bibletimeapp.cpp
@@ -11,6 +11,7 @@
 
 #include <QDebug>
 #include <QPainter>
+#include <QtGlobal>
 #include "frontend/messagedialog.h"
 #include "backend/config/btconfig.h"
 #include "backend/managers/cswordbackend.h"
@@ -25,8 +26,11 @@ BibleTimeApp::BibleTimeApp(int &argc, char **argv)
 {
     setApplicationName("bibletime");
     setApplicationVersion(BT_VERSION);
+
     // Support for retina displays
+    #if QT_VERSION >= 0x050200
     this->setAttribute(Qt::AA_UseHighDpiPixmaps);
+    #endif
 }
 
 BibleTimeApp::~BibleTimeApp() {