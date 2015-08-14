f86324e429ae7e82d97ec83963f8701482c0ac16
Jaak Ristioja <jaak@ristioja.ee>
2015-08-14 14:11:39 +0300

    Implemented BtConstModuleSet and BtModuleSet.

---
+++ src/backend/drivers/btconstmoduleset.h
@@ -0,0 +1,35 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2014 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef BTCONSTMODULESET_H
+#define BTCONSTMODULESET_H
+
+#include <QSet>
+
+
+class CSwordModuleInfo;
+
+class BtConstModuleSet: public QSet<CSwordModuleInfo const *> {
+
+public: /* Methods: */
+
+    BtConstModuleSet(QSet<CSwordModuleInfo const *> const & copy)
+        : QSet<CSwordModuleInfo const *>(copy)
+    {}
+
+    BtConstModuleSet(QSet<CSwordModuleInfo *> other) {
+        Q_FOREACH(CSwordModuleInfo const * const item, other)
+            insert(item);
+    }
+
+};
+
+#endif /* BTCONSTMODULESET_H */
---
+++ src/backend/drivers/btmoduleset.h
@@ -0,0 +1,22 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2014 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef BTMODULESET_H
+#define BTMODULESET_H
+
+#include <QSet>
+
+
+class CSwordModuleInfo;
+
+typedef QSet<CSwordModuleInfo *> BtModuleSet;
+
+#endif /* BTMODULESET_H */
