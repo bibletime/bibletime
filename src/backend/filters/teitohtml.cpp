a7fa71b2b6dc80d53bb85c92caf6e836009e6d0e
Jaak Ristioja <jaak@ristioja.ee>
2015-09-04 23:45:19 +0300

    TeiToHtml: Minor refactoring.

--- src/backend/filters/teitohtml.cpp
+++ src/backend/filters/teitohtml.cpp
@@ -126,23 +126,23 @@ void TeiToHtml::renderReference(const char *osisRef, sword::SWBuf &buf,
         }
 
         if (mod) {
-            ReferenceManager::ParseOptions options;
-            options.refBase = QString::fromUtf8(myUserData->key->getText());
-            options.refDestinationModule = QString(mod->name());
-            options.sourceLanguage = QString(mod->module()->getLanguage());
-            options.destinationLanguage = QString("en");
-
-            buf.append("<a href=\"");
-            buf.append( //create the hyperlink with key and mod
-                ReferenceManager::encodeHyperlink(
+            ReferenceManager::ParseOptions const options(
                     mod->name(),
-                    ReferenceManager::parseVerseReference(hrefRef, options),
-                    ReferenceManager::typeFromModule(mod->type())
-                ).toUtf8().constData()
-            );
-            buf.append("\" crossrefs=\"");
-            buf.append(ReferenceManager::parseVerseReference(ref, options).toUtf8().constData()); //ref must contain the osisRef module marker if there was any
-            buf.append("\">");
+                    QString::fromUtf8(myUserData->key->getText()),
+                    mod->module()->getLanguage());
+
+            buf.append("<a href=\"")
+               .append( // create the hyperlink with key and mod
+                    ReferenceManager::encodeHyperlink(
+                        mod->name(),
+                        ReferenceManager::parseVerseReference(hrefRef, options),
+                        ReferenceManager::typeFromModule(mod->type())
+                    ).toUtf8().constData()
+                )
+               .append("\" crossrefs=\"")
+                // ref must contain the osisRef module marker if there was any:
+               .append(ReferenceManager::parseVerseReference(ref, options).toUtf8().constData())
+               .append("\">");
         }
         // should we add something if there were no referenced module available?
     }
