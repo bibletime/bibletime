1b41db139be24a0d117a4ab3ce23ec7e09ebbae1
Jaak Ristioja <jaak@ristioja.ee>
2015-11-21 15:37:53 +0200

    util: Added some helper macros.
    In some detail,
      * BT_CONNECT* macros help to assert return status of QObject::connect().
      * BT_SCOPE_EXIT will run code when leaving the current scope.
      * BT_STRING_JOIN will help join strings during preprocessing.
      * BT_WRAP helps wrap arguments passed through multiple levels of macros.

---
+++ src/util/btconnect.h
@@ -0,0 +1,34 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/.
+*
+* Copyright 1999-2015 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License version 2.0.
+*
+**********/
+
+#ifndef BTCONNECT_H
+#define BTCONNECT_H
+
+#include <cassert>
+#include <QObject>
+#include "btwrap.h"
+
+
+#ifndef NDEBUG
+#define BT_CONNECT_WITH(with, ...) \
+    do { \
+        bool const ok = with connect(__VA_ARGS__); \
+        assert(ok); \
+    } while (false)
+#define BT_CONNECT(...)         BT_CONNECT_WITH(,         BT_WRAP(__VA_ARGS__))
+#define BT_CONNECT_QOBJECT(...) BT_CONNECT_WITH(QObject::,BT_WRAP(__VA_ARGS__))
+#else
+#define BT_CONNECT_WITH(with, ...)  with connect(__VA_ARGS__)
+#define BT_CONNECT(...)                  connect(__VA_ARGS__)
+#define BT_CONNECT_QOBJECT(...) QObject::connect(__VA_ARGS__)
+#endif
+
+#endif /* BTCONNECT_H */
