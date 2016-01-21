57ede677712101088437ad8585d6ad7d9627f131
Gary Holmlund <gary.holmlund@gmail.com>
2016-01-21 14:49:25 -0800

    In bookshelf wizard, if multiple languages selected and grouping is currently "cat", change to "cat/lang"

--- src/frontend/bookshelfwizard/btbookshelfworkspage.cpp
+++ src/frontend/bookshelfwizard/btbookshelfworkspage.cpp
@@ -218,8 +218,12 @@ void BtBookshelfWorksPage::initializePage() {
 }
 
 void BtBookshelfWorksPage::setGrouping() {
+    BtBookshelfTreeModel::Grouping cat(BtBookshelfTreeModel::GROUP_CATEGORY);
+    BtBookshelfTreeModel::Grouping catLang;
     if (m_languages.count() == 1)
-        slotGroupingActionTriggered(BtBookshelfTreeModel::Grouping(BtBookshelfTreeModel::GROUP_CATEGORY));
+        slotGroupingActionTriggered(cat);
+    else if (m_languages.count() > 1 && m_groupingOrder == cat)
+            slotGroupingActionTriggered(catLang);
 }
 
 void BtBookshelfWorksPage::updateModels() {
