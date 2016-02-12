ec539ee7555b25f810662e225ae517f2c4527300
Martin Gruner <mgruner@crosswire.org>
2016-02-12 20:43:48 +0100

    Added new Japanese translation for mobile.

---
+++ i18n/messages/mobile_ui_ja.ts
@@ -0,0 +1,424 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ja" version="2.1">
+<context>
+    <name/>
+    <message>
+        <source/>
+        <translatorcomment>Bibletime User Interface translation
+Copyright (C) 1999-2015 The Bibletime Team, http://www.bibletime.info
+This file is distributed under the same license as the Bibetime package.</translatorcomment>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../../src/mobile/qml/About.qml" line="54"/>
+        <source>BibleTime Mobile</source>
+        <translation>BibleTime Mobile</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/About.qml" line="76"/>
+        <source>Brought to you by the</source>
+        <translation>提供主</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/About.qml" line="77"/>
+        <source>Version</source>
+        <translation>バージョン</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/About.qml" line="81"/>
+        <source>Qt version</source>
+        <translation>Qt バージョン</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/About.qml" line="85"/>
+        <source>Sword version</source>
+        <translation>Sword バージョン</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/About.qml" line="89"/>
+        <source>Licensed Under the</source>
+        <translation>利用許諾契約</translation>
+    </message>
+</context>
+<context>
+    <name>FontSizeSlider</name>
+    <message>
+        <location filename="../../src/mobile/qml/FontSizeSlider.qml" line="121"/>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/FontSizeSlider.qml" line="138"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+</context>
+<context>
+    <name>Install Documents</name>
+    <message>
+        <location filename="../../src/mobile/qml/ListWorksView.qml" line="111"/>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/ListWorksView.qml" line="113"/>
+        <source>Install</source>
+        <translation>インストール</translation>
+    </message>
+</context>
+<context>
+    <name>InstallManagerChooser</name>
+    <message>
+        <location filename="../../src/mobile/qml/InstallManagerChooser.qml" line="73"/>
+        <source>Source</source>
+        <translation>ソース</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/InstallManagerChooser.qml" line="81"/>
+        <source>Category</source>
+        <translation>カテゴリ</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/InstallManagerChooser.qml" line="92"/>
+        <source>Language</source>
+        <translation>言語</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/InstallManagerChooser.qml" line="104"/>
+        <source>Document</source>
+        <translation>文書</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/InstallManagerChooser.qml" line="134"/>
+        <source>Refresh Sources</source>
+        <translation>ソースの更新</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/InstallManagerChooser.qml" line="151"/>
+        <source>Install / Remove</source>
+        <translation>インストール/削除</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolbar</name>
+    <message>
+        <location filename="../../src/mobile/qml/MainToolbar.qml" line="55"/>
+        <source>BibleTime Mobile</source>
+        <translation>BibleTime Mobile</translation>
+    </message>
+</context>
+<context>
+    <name>ModuleChooser</name>
+    <message>
+        <location filename="../../src/mobile/qml/ModuleChooser.qml" line="121"/>
+        <source>Unlock</source>
+        <translation>ロック解除</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/ModuleChooser.qml" line="170"/>
+        <source>Category</source>
+        <translation>カテゴリ</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/ModuleChooser.qml" line="181"/>
+        <source>Language</source>
+        <translation>言語</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/ModuleChooser.qml" line="193"/>
+        <source>Work</source>
+        <translation>文献</translation>
+    </message>
+</context>
+<context>
+    <name>Progress</name>
+    <message>
+        <location filename="../../src/mobile/qml/Progress.qml" line="66"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+</context>
+<context>
+    <name>Question</name>
+    <message>
+        <location filename="../../src/mobile/qml/Question.qml" line="86"/>
+        <source>Yes</source>
+        <translation>はい</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/Question.qml" line="105"/>
+        <source>No</source>
+        <translation>いいえ</translation>
+    </message>
+</context>
+<context>
+    <name>Quit</name>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="495"/>
+        <source>Are you sure you want to quit?</source>
+        <translation>ほんとうに終了しますか?</translation>
+    </message>
+</context>
+<context>
+    <name>Search</name>
+    <message>
+        <location filename="../../src/mobile/qml/Search.qml" line="71"/>
+        <location filename="../../src/mobile/qml/Search.qml" line="119"/>
+        <source>Search</source>
+        <translation>検索</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/Search.qml" line="147"/>
+        <source>Find</source>
+        <translation>検索</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/Search.qml" line="165"/>
+        <source>All Words</source>
+        <translation>完全一致</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/Search.qml" line="181"/>
+        <source>Any Word</source>
+        <translation>部分一致</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/Search.qml" line="196"/>
+        <source>Regular Expression</source>
+        <translation>正規表現</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/Search.qml" line="222"/>
+        <source>Search in</source>
+        <translation>検索対象</translation>
+    </message>
+</context>
+<context>
+    <name>SearchResults</name>
+    <message>
+        <location filename="../../src/mobile/qml/SearchResults.qml" line="127"/>
+        <source>Documents</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/SearchResults.qml" line="158"/>
+        <source>References</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SetFont</name>
+    <message>
+        <location filename="../../src/mobile/qml/SetFont.qml" line="110"/>
+        <source>For Language</source>
+        <translation>対象言語</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/SetFont.qml" line="145"/>
+        <source>Font</source>
+        <translation>フォント</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/SetFont.qml" line="180"/>
+        <source>Font Size</source>
+        <translation>フォントサイズ</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/SetFont.qml" line="230"/>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/SetFont.qml" line="247"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+</context>
+<context>
+    <name>StartupBookshelfManager</name>
+    <message>
+        <location filename="../../src/mobile/qml/StartupBookshelfManager.qml" line="26"/>
+        <source>There are currently no documents installed. Please click on the Ok button below to install new documents.</source>
+        <translation>現在、文書がインストールされていません。[OK]をクリックして新規文書をインストールしてください。</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/StartupBookshelfManager.qml" line="51"/>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/StartupBookshelfManager.qml" line="72"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+</context>
+<context>
+    <name>Welcome</name>
+    <message>
+        <location filename="../../src/mobile/qml/WindowManager.qml" line="265"/>
+        <source>Use the &quot;New Window&quot; menu to open a document.</source>
+        <translation>[新規ウィンドウ]メニューから文書を開いてください。</translation>
+    </message>
+</context>
+<context>
+    <name>btm::BtSearchInterface</name>
+    <message>
+        <location filename="../../src/mobile/ui/btsearchinterface.cpp" line="58"/>
+        <source>Indexing</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>btm::InstallProgress</name>
+    <message>
+        <location filename="../../src/mobile/bookshelfmanager/installprogress.cpp" line="96"/>
+        <source>Installing</source>
+        <translation>インストール中</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/bookshelfmanager/installprogress.cpp" line="102"/>
+        <source>Downloading</source>
+        <translation>ダウンロード中</translation>
+    </message>
+</context>
+<context>
+    <name>btm::InstallSources</name>
+    <message>
+        <location filename="../../src/mobile/bookshelfmanager/installsources.cpp" line="55"/>
+        <source>Refreshing</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/mobile/bookshelfmanager/installsources.cpp" line="62"/>
+        <source>Failed to refresh source %1</source>
+        <translation>ソース %1 が更新できませんでした</translation>
+    </message>
+</context>
+<context>
+    <name>btm::InstallSourcesManager</name>
+    <message>
+        <location filename="../../src/mobile/bookshelfmanager/installsourcesmanager.cpp" line="45"/>
+        <source>Refreshing Source List</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>btm::VerseChooser</name>
+    <message>
+        <location filename="../../src/mobile/keychooser/versechooser.cpp" line="52"/>
+        <source>Book</source>
+        <translation>巻</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/keychooser/versechooser.cpp" line="61"/>
+        <source>Chapter</source>
+        <translation>章(篇)</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/keychooser/versechooser.cpp" line="68"/>
+        <source>Verse</source>
+        <translation>節</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="233"/>
+        <source>Some of the modules you want to search need to be indexed. Do you want to index them now?</source>
+        <translation>検索するモジュールのいくつかはインデックスが必要です。インデックスを付与しますか?</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="321"/>
+        <source>New Window</source>
+        <translation>新規ウィンドウ</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="322"/>
+        <source>View Window</source>
+        <translation>ウインドウ表示</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="323"/>
+        <source>Close Window</source>
+        <translation>ウィンドウを閉じます</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="326"/>
+        <location filename="../../src/mobile/qml/main.qml" line="476"/>
+        <source>User Interface Font Size</source>
+        <translation>ユーザー インターフェイスのフォントサイズ</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="327"/>
+        <source>Window Arrangement</source>
+        <translation>ウィンドウを整列します</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="324"/>
+        <source>Manage Installed Documents</source>
+        <translation>インストールした文書を管理します</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="329"/>
+        <source>About</source>
+        <translation>情報</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="418"/>
+        <source>Dark</source>
+        <translation>濃色</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="419"/>
+        <source>Light Blue</source>
+        <translation>水色</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="420"/>
+        <source>Crimson</source>
+        <translation>深紅色</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="381"/>
+        <source>Single</source>
+        <translation>単一</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="178"/>
+        <source>BibleTime views documents such as Bibles and commentaries. These documents are downloaded and stored locally.There are currently no documents. Do you want to install documents now?</source>
+        <translation>BibleTimeは聖書や注解書などの文書を表示します。これらの文書はダウンロードされ、ローカルに保存されます。現在、文書はありません。文書をダウンロードしますか?</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="325"/>
+        <source>Text Font</source>
+        <translation>本文フォント</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="328"/>
+        <source>Color Theme</source>
+        <translation>配色テーマ</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="382"/>
+        <source>Tabbed</source>
+        <translation>タブを付与します</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="383"/>
+        <source>Auto-tile</source>
+        <translation>自動で並べます</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="384"/>
+        <source>Auto-tile horizontally</source>
+        <translation>自動で水平方向に並べます</translation>
+    </message>
+    <message>
+        <location filename="../../src/mobile/qml/main.qml" line="385"/>
+        <source>Auto-tile vertically</source>
+        <translation>自動で垂直方向に並べます</translation>
+    </message>
+</context>
+</TS>
