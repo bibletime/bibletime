ff1936d19c408561543fc07c30408701b7671b5f
Jaak Ristioja <jaak@ristioja.ee>
2017-11-05 21:11:56 +0200

    Backend, CTextRendering: Fixed Doxygen comments.

--- src/backend/rendering/ctextrendering.h
+++ src/backend/rendering/ctextrendering.h
@@ -55,12 +55,12 @@ class CTextRendering {
                 struct Settings {
 
                     enum KeyRenderingFace {
-                        NoKey, //< means no key shown at all
-                        SimpleKey, //< means only versenumber or only lexicon entry name
-                        CompleteShort, //< means key like "Gen 1:1"
-                        CompleteLong, //< means "Genesis 1:1"
-                        ExpandedShort, // means "KJV:Gen 1:1"
-                        ExpandedLong   // means "Genesis 1:1 (KJV)"
+                        NoKey,         ///< means no key shown at all
+                        SimpleKey,     ///< means only versenumber or only lexicon entry name
+                        CompleteShort, ///< means key like "Gen 1:1"
+                        CompleteLong,  ///< means "Genesis 1:1"
+                        ExpandedShort, /// means "KJV:Gen 1:1"
+                        ExpandedLong   /// means "Genesis 1:1 (KJV)"
                     };
 
                     Settings(const bool highlight = false,
