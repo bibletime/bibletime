d8766cd226b9fa803b07926dea54f51cfee3f4c2
Jaak Ristioja <jaak@ristioja.ee>
2019-11-26 08:49:44 +0200

    cmake/fix_cpp_header.sh: Fixed temp file leak, whitespace handling

--- cmake/fix_cpp_header.sh
+++ cmake/fix_cpp_header.sh
@@ -1,26 +1,57 @@
 #!/bin/sh
+set -euo pipefail
+
+ON_EXIT_ITEMS=()
+
+on_exit() {
+    while [ ${#ON_EXIT_ITEMS[*]} -ne 0 ]; do
+        eval "${ON_EXIT_ITEMS[${#ON_EXIT_ITEMS[@]}-1]}"
+        unset ON_EXIT_ITEMS[${#ON_EXIT_ITEMS[@]}-1]
+    done
+}
+
+add_on_exit() {
+    local N=${#ON_EXIT_ITEMS[*]}
+    ON_EXIT_ITEMS["${N}"]="$*"
+    if [[ "${N}" -eq 0 ]]; then
+        trap on_exit EXIT
+    fi
+}
+
 TARGETFILE="$1"
 HEADER="$2"
 TMPFILE=$(mktemp)
+add_on_exit "rm -f '${TMPFILE}'"
+
+cat "$HEADER" > "$TMPFILE"
+
 awk '
-  BEGIN { a = 0 }
+  BEGIN { state = 0 }
 
   /^\/\*/ {
-    if (a == 0)
-      a = 1
+    if (state == 0) {
+      state = 1
+    }
   }
+
   /\*\// {
-    if (a == 1)
-      a = 2
+    if (state == 1) {
+      $0 = substr($0, index($0, "*/") + 2)
+      sub(/^[[:blank:]]*/, "")
+      state = 2
+    }
   }
 
   {
-    if (a == 2) {
-      a = 3
-    } else if (a == 3) {
+    if (state != 1) {
       print
+      state = 2
     }
   }
-' "$TARGETFILE" > "$TMPFILE"
-cat "$HEADER" "$TMPFILE" > "$TARGETFILE"
-rm "$TMPFILE"
+' "$TARGETFILE" | \
+awk '
+  BEGIN { state = 0 }
+  /^[[:blank:]]*$/ { if (state == 1) print }
+  /[^[:blank:]]/ { state = 1; print }
+' >> "$TMPFILE"
+cp -f "$TMPFILE" "$TARGETFILE"
