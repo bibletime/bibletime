0cfe7ade586072b5eac0cd12a77681112dbea93c
Jaak Ristioja <jaak@ristioja.ee>
2020-01-02 11:53:29 +0200

    make fix_cpp_headers

--- src/frontend/btaboutdialog.h
+++ src/frontend/btaboutdialog.h
@@ -4,7 +4,7 @@
 *
 * This file is part of BibleTime's source code, http://www.bibletime.info/
 *
-* Copyright 1999-2019 by the BibleTime developers.
+* Copyright 1999-2020 by the BibleTime developers.
 * The BibleTime source code is licensed under the GNU General Public License
 * version 2.0.
 *
