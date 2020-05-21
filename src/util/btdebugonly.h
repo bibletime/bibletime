ec5d6c80be24d4be15983b4c4d8317bcad610ebf
Jaak Ristioja <jaak@ristioja.ee>
2020-05-22 00:49:23 +0300

    util: Implemented BT_DEBUG_ONLY() and BT_NDEBUG_ONLY() macros

---
+++ src/util/btdebugonly.h
@@ -0,0 +1,24 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, http://www.bibletime.info/
+*
+* Copyright 1999-2020 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License
+* version 2.0.
+*
+**********/
+
+#ifndef BTDEBUGONLY_H
+#define BTDEBUGONLY_H
+
+#ifdef NDEBUG
+#define BT_DEBUG_ONLY(...)
+#define BT_NDEBUG_ONLY(...) __VA_ARGS__
+#else
+#define BT_DEBUG_ONLY(...) __VA_ARGS__
+#define BT_NDEBUG_ONLY(...)
+#endif
+
+#endif /* BTDEBUGONLY_H */
