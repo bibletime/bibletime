6cf9163f11b60b247d7c0f77911f4f43b8bbb920
Jaak Ristioja <jaak@ristioja.ee>
2020-07-20 01:44:25 +0300

    cmake/BTApplication.cmake: Fixed tests being commented out

--- cmake/BTApplication.cmake
+++ cmake/BTApplication.cmake
@@ -157,7 +157,7 @@ INCLUDE("${CMAKE_CURRENT_SOURCE_DIR}/cmake/BTUITranslation.cmake")
 ######################################################
 # Tests:
 #
-#INCLUDE("${CMAKE_CURRENT_SOURCE_DIR}/cmake/BTTests.cmake")
+INCLUDE("${CMAKE_CURRENT_SOURCE_DIR}/cmake/BTTests.cmake")
 
 
 ######################################################
