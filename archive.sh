6fbd688c80c258c94d44181eca69ffab94d740ec
Jaak Ristioja <jaak@ristioja.ee>
2021-02-22 00:15:22 +0200

    archive.sh: Refactored, create .tar.lz and GPG signatures as well

--- archive.sh
+++ archive.sh
@@ -1,11 +1,34 @@
-#!/bin/sh
-FULLNAME="bibletime-$1"
-TARXZNAME="${FULLNAME}.tar.xz"
-git archive --format=tar "--prefix=${FULLNAME}/" HEAD | xz -9e > "$TARXZNAME" || exit 1
-BYTES=`stat '--format=%s' "$TARXZNAME"`
-echo 'Details:'
-echo "  Filename:  ${TARXZNAME}"
-echo "  File size: $BYTES bytes`echo $BYTES|awk '{s[2**30]="G";s[2**20]="M";hum[1024]="k";for(x=2**30;x>=1024;x/=1024){if($1>=x){printf " (%.2f %sB)\n",$1/x,s[x];break}}}'`"
-echo "  MD5SUM:    `md5sum \"${TARXZNAME}\" |cut -b -32`"
-echo "  SHA1SUM:   `sha1sum \"${TARXZNAME}\" |cut -b -40`"
-echo "  SHA256SUM: `sha256sum \"${TARXZNAME}\" |cut -b -64`"
+#!/bin/bash
+set -euo pipefail
+
+FULLNAME="bibletime-${1:-custom}"
+
+# Use a temporary directory:
+TMPDIR=$(mktemp -d)
+# shellcheck disable=SC2064
+trap "rm -rf '${TMPDIR}'" EXIT
+
+make_tarball() {
+    COMPRESSOR="$1"
+    OUT="${TMPDIR}/$2"
+    git archive --format=tar "--prefix=${FULLNAME}/" HEAD | ${COMPRESSOR} > "${OUT}" || exit 1
+    BYTES=$(stat '--format=%s' "${OUT}")
+    PRETTY_BYTES=$(echo "${BYTES}" | awk '{s[2**30]="G";s[2**20]="M";hum[1024]="k";for(x=2**30;x>=1024;x/=1024){if($1>=x){printf "%.2f %sB",$1/x,s[x];break}}}')
+    MD5SUM=$(md5sum "${OUT}"|cut -b -32)
+    SHA1SUM=$(sha1sum "${OUT}"|cut -b -40)
+    SHA256SUM=$(sha256sum "${OUT}"|cut -b -64)
+    cat << END_OF_DETAILS
+
+Details of $2:
+  File size: ${BYTES} bytes (${PRETTY_BYTES})
+  MD5SUM:    ${MD5SUM}
+  SHA1SUM:   ${SHA1SUM}
+  SHA256SUM: ${SHA256SUM}
+END_OF_DETAILS
+    mv -i "${OUT}" .
+    gpg --armor --detach-sig "$2"
+}
+
+echo "Creating archives for ${FULLNAME}..."
+make_tarball 'xz -9e' "${FULLNAME}.tar.xz"
+make_tarball 'lzip --best' "${FULLNAME}.tar.lz"
