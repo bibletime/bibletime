cc1d920c8b3a280b18dfb03eaf2c81870699c425
Jaak Ristioja <jaak@ristioja.ee>
2021-04-19 23:14:35 +0300

    util: Add cp1252() implementation forgotten by previous commit

---
+++ src/util/cp1252.cpp
@@ -0,0 +1,27 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, https://bibletime.info/
+*
+* Copyright 1999-2021 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License
+* version 2.0.
+*
+**********/
+
+#include "cp1252.h"
+
+#include <QTextCodec>
+#include "btassert.h"
+
+
+namespace util {
+
+QTextCodec const & cp1252() {
+    static auto const * const codec = QTextCodec::codecForName("Windows-1252");
+    BT_ASSERT(codec);
+    return *codec;
+}
+
+} /* namespace util { */
---
+++ src/util/cp1252.h
@@ -0,0 +1,21 @@
+/*********
+*
+* In the name of the Father, and of the Son, and of the Holy Spirit.
+*
+* This file is part of BibleTime's source code, https://bibletime.info/
+*
+* Copyright 1999-2021 by the BibleTime developers.
+* The BibleTime source code is licensed under the GNU General Public License
+* version 2.0.
+*
+**********/
+
+#pragma once
+
+class QTextCodec;
+
+namespace util {
+
+QTextCodec const & cp1252();
+
+} /* namespace util { */
