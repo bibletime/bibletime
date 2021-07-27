bibletime/bibletime

commit ebc370cd9d228f8477bb651be13b62d656b71ac8

frontend, CDisplayWindow: Inlined pageDown() and pageUp()

src/frontend/displaywindow/cdisplaywindow.cpp
@@ -78,7 +78,7 @@ CDisplayWindow::CDisplayWindow(const QList<CSwordModuleInfo *> & modules, CMDIAr
     setObjectName("CDisplayWindow");
     m_actionCollection = new BtActionCollection(this);
 
-    for (auto const * const mod : newModules)
+    for (auto const * const mod : modules)
         m_modules.append(mod->name());
 
     // Connect this to the backend module list changes
@@ -300,8 +300,16 @@ void CDisplayWindow::initActions() {
                this,
                &CDisplayWindow::slotSearchInModules);
     initAddAction("openLocation", this, &CDisplayWindow::setFocusKeyChooser);
-    initAddAction("pageDown", this, &CDisplayWindow::pageDown);
-    initAddAction("pageUp", this, &CDisplayWindow::pageUp);
+    initAddAction("pageDown",
+                  [this]{
+                      if (m_displayWidget)
+                          m_displayWidget->pageUp();
+                  });
+    initAddAction("pageUp",
+                  [this]{
+                      if (m_displayWidget)
+                          m_displayWidget->pageUp();
+                  });
 
     initAddAction("copySelectedText",
                   m_displayWidget,
@@ -849,16 +857,6 @@ void CDisplayWindow::setFocusKeyChooser() {
         if (mainWinKeyChooser)
             mainWinKeyChooser->setFocus();
     }
-}
-
-void CDisplayWindow::pageDown() {
-    if (m_displayWidget)
-        m_displayWidget->pageDown();
-}
-
-void CDisplayWindow::pageUp() {
-    if (m_displayWidget)
-        m_displayWidget->pageUp();
 }
 
 /** This function saves the entry as html using the CExportMgr class. */

src/frontend/displaywindow/cdisplaywindow.h
@@ -267,10 +267,6 @@ protected Q_SLOTS:
 
     void setFocusKeyChooser();
 
-    void pageDown();
-
-    void pageUp();
-
     /**
         * This function saves the entry as html using the CExportMgr class.
         */

commit fae895d89190a5f03b892df5aa557e4ad4b37c62

frontend, CDisplayWindow: Fixed page down triggering pageUp() on the display
Was broken by ebc370cd9d228f8477bb651be13b62d656b71ac8, not part of any release.

src/frontend/displaywindow/cdisplaywindow.cpp
@@ -303,7 +303,7 @@ void CDisplayWindow::initActions() {
     initAddAction("pageDown",
                   [this]{
                       if (m_displayWidget)
-                          m_displayWidget->pageUp();
+                          m_displayWidget->pageDown();
                   });
     initAddAction("pageUp",
                   [this]{