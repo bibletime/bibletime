6b5fdb0881c7e0d2189ce3822a6eba6d8bb6ba5c
Jaak Ristioja <jaak@ristioja.ee>
2021-08-30 21:14:57 +0300

    backend, ThmlToHtml: Don't pass default position parameter to indexIn() in processText()

--- src/backend/filters/thmltohtml.cpp
+++ src/backend/filters/thmltohtml.cpp
@@ -92,7 +92,7 @@ char ThmlToHtml::processText(sword::SWBuf &buf, const sword::SWKey *key,
     for (auto & e : list) {
 
         // pass text ahead of <sync> stright through
-        if (auto const pos = tag.indexIn(e, 0); pos >= 0) {
+        if (auto const pos = tag.indexIn(e); pos >= 0) {
             result.append(e.left(pos));
             e.remove(0, pos);
         }
@@ -101,7 +101,7 @@ char ThmlToHtml::processText(sword::SWBuf &buf, const sword::SWKey *key,
         bool hasLemmaAttr = false;
         bool hasMorphAttr = false;
 
-        int pos = tag.indexIn(e, 0);
+        int pos = tag.indexIn(e);
         bool insertedTag = false;
         QString value;
         QString valueClass;
