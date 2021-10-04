ba27b4e328a65b103c7dd9a69cf0d079bb148286
Gary Holmlund <gw@holmlund.org>
2021-10-04 07:49:19 -0700

    Change macOS sdk version to 12.4

--- cmake/BTApplication.cmake
+++ cmake/BTApplication.cmake
@@ -59,7 +59,7 @@ ELSE()
   SET(CMAKE_CXX_FLAGS_RELEASE "")
   SET(CMAKE_CXX_FLAGS_DEBUG "")
   IF(APPLE)
-    SET(T "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/")
+    SET(T "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.4.sdk/")
     LIST(APPEND BibleTime_CXXFLAGS
         "-mmacosx-version-min=10.12"
         "-stdlib=libc++"
