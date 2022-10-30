8e449eaa95a16b162363bcd925efc70eeed5093b
Jaak Ristioja <jaak@ristioja.ee>
2022-10-30 19:57:54 +0200

    cmake/BTUITranslation.cmake: Create and install empty *.qm file for en_US
    This ensures that Qt "knows" BibleTime supports en_US. If the default system
    language is en_US, but other languages are supported as well, QTranslator first
    searches for any en_US translation files and then those of other languages.
    Since BibleTime does not provide an en_US translation file by default, Qt might
    end up using another translation file for an alternative language, ignoring the
    default en_US language.

--- cmake/BTUITranslation.cmake
+++ cmake/BTUITranslation.cmake
@@ -32,6 +32,23 @@ ADD_CUSTOM_TARGET("messages_default" COMMAND
 SET_TARGET_PROPERTIES("messages_default" PROPERTIES FOLDER "Messages")
 ADD_DEPENDENCIES("messages" "messages_default")
 
+MESSAGE(STATUS "Creating empty translation files for default en_US language...")
+SET(TS_FILE "${CMAKE_CURRENT_BINARY_DIR}/bibletime_ui_en_US.ts")
+SET(TS_QM_FILE "${CMAKE_CURRENT_BINARY_DIR}/bibletime_ui_en_US.qm")
+FILE(WRITE "${TS_FILE}" [=[
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US"></TS>
+]=])
+EXECUTE_PROCESS(
+    COMMAND "${QT_LRELEASE_EXECUTABLE}" "${TS_FILE}" "-qm" "${TS_QM_FILE}"
+    RESULT_VARIABLE r
+)
+IF(NOT ("${r}" EQUAL 0))
+    MESSAGE(FATAL_ERROR "Failed to run ${QT_LRELEASE_EXECUTABLE}!")
+ENDIF()
+INSTALL(FILES "${TS_QM_FILE}" DESTINATION "${BT_LOCALEDIR}/")
+
 FOREACH(TS_FILE IN LISTS TS_FILES)
     STRING(REGEX REPLACE "${TS_DIR}/${TS_PREFIX}(...?(_..)?).ts" "\\1"
            TS_LANG "${TS_FILE}")
