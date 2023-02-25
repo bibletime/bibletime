bibletime/bibletime

commit cb26d31d378aa11f46b8c002a5de23228f5fd369

frontend, BtModuleChooserBar: Refactored adjustButtonCount()

src/frontend/displaywindow/btmodulechooserbar.cpp
@@ -36,18 +36,16 @@ void BtModuleChooserBar::setModules(BtModuleList newModules) {
 }
 
 void BtModuleChooserBar::adjustButtonCount() {
-    int buttonCountDifference = m_buttonList.count() - (m_modules.count() + 1);
-    //if there are more buttons than modules, delete buttons
-    if (buttonCountDifference > 0) {
-        for (int j = 0; j < buttonCountDifference; j++) {
+    if (m_buttonList.size() > m_modules.size()) {
+        auto toRemove = m_buttonList.size() - m_modules.size();
+        do {
             delete m_buttonList.takeFirst();
-        }
-    }
-    // if there are more modules than buttons, add buttons
-    if (buttonCountDifference < 0) {
-        for (int i = (buttonCountDifference * (-1)); i > 0; i--) {
+        } while (--toRemove);
+    } else if (m_modules.size() > m_buttonList.size()) {
+        auto toAdd = m_modules.size() - m_buttonList.size();
+        do {
             addButton();
-        }
+        } while (--toAdd);
     }
 }
 

commit 6852b49a88d82972d3f9e15a5538a2597a646d4f

frontend, BtModuleChooserBar: Fixed add button not being shown on update
When modules change in the associated window, the add button was hidden. The bug
was introduced by commit cb26d31d378aa11f46b8c002a5de23228f5fd369 earlier today.

src/frontend/displaywindow/btmodulechooserbar.cpp
@@ -30,7 +30,7 @@ BtModuleChooserBar::BtModuleChooserBar(QWidget * parent)
 
 void BtModuleChooserBar::setModules(BtModuleList modules) {
     if (m_moduleType != CSwordModuleInfo::GenericBook)
-        adjustButtonCount(modules.size());
+        adjustButtonCount(modules.size() + 1);
     updateButtonMenus(modules);
 }
 