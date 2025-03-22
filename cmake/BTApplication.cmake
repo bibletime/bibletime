977b581af199898e65e6bfa5a01aab4118b78c78
Jaak Ristioja <jaak@ristioja.ee>
2025-03-22 13:07:48 +0200

    cmake: Removed uninstall target

--- cmake/BTApplication.cmake
+++ cmake/BTApplication.cmake
@@ -221,12 +221,3 @@ ENDIF()
 IF(WIN32 AND NOT UNIX)
   INCLUDE("${CMAKE_CURRENT_SOURCE_DIR}/cmake/BTBinaryPackageWindows.cmake")
 ENDIF()
-
-
-######################################################
-# "make uninstall" target; see http://www.cmake.org/Wiki/CMake_FAQ#Can_I_do_.22make_uninstall.22_with_CMake.3F
-#
-CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
-               "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
-               IMMEDIATE @ONLY)
-ADD_CUSTOM_TARGET(uninstall "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake")
